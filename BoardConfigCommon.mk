f7daea581d45f5d9691135e7ac5c971c6002a545
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-04-12 22:57:17 +0500

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,7 +12,7 @@ COMMON_PATH := device/xiaomi/trinket-common
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_CPU_ABI2 := cell-window2-sill
 TARGET_CPU_VARIANT := cortex-a73
 
 TARGET_2ND_ARCH := arm
