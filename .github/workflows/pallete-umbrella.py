a789c0bf55f77c4f8fc4ef7415ec8324ef98248f
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-08-07 16:49:19 +0500

    Update pallete-umbrella.py

--- .github/workflows/pallete-umbrella.py
+++ .github/workflows/pallete-umbrella.py
@@ -1,6 +1,116 @@
 name: Python Package using Conda
 
-on: [push]
+on: [ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webpaint &mdash; digital &amp; branding agency</title>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/img/favicon.ico">
+    <link rel="stylesheet" href="/css/custom-fonts.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/adaptive.css">
+</head>
+<body>
+    <header>
+        <div class="header-container center">
+            <a href="/home.html" class="logo"></a>
+            <nav class="menu">
+                <div class="menu-item active"><a href="home.html">home</a></div>
+                <div class="menu-item"><a href="portfolio.html">portfolio</a></div>
+                <div class="menu-item"><a href="about.html">about</a></div>
+                <div class="menu-item"><a href="blog.html">blog</a></div>
+                <div class="menu-item"><a href="contact.html">contact</a></div>
+            </nav>
+        </div>
+    </header>
+    <section class="slider">
+        <div class="center">
+            <h1>We are <strong>Webpaint</strong></h1>
+            <h2><em>digital &amp; branding</em> agency based in Jupiter and we would love to turn ideas into beautiful things</h2>
+            <a href="#portfolio" class="button-red button-big">see portfolio</a>
+        </div>
+    </section>
+    <section class="services">
+        <div class="services-container center">
+            <div class="service-item">
+                <div class="service-icon service-icon-1"></div>
+                <div class="service-name">Consectetur</div>
+                <div class="service-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iusto, quod expedita tempora placeat sequi dolor architecto.
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-icon service-icon-2"></div>
+                <div class="service-name">Tristiquet</div>
+                <div class="service-description">
+                Culpa recusandae officiis, minus delectus facere iste dolores veniam dolorum ea, impedit distinctio voluptates molestiae qui.
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-icon service-icon-3"></div>
+                <div class="service-name">Fermentum</div>
+                <div class="service-description">
+                Assumenda, voluptatum fugit? Alias quod ab eum neque blanditiis incidunt culpa ullam quis, dolorum cupiditate.
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-icon service-icon-4"></div>
+                <div class="service-name">Elit Ultricies</div>
+                <div class="service-description">
+                Amet debitis vitae quae excepturi cumque totam esse labore cupiditate, culpa fugiat, ut illo velit dignissimos adipisci.
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="portfolio">
+        <div class="center clearfix">
+            <h3>Our Featured Works</h3>
+            <h4>Sed sequi, maxime nisi consequuntur, illum asperiores repudiandae.</h4>
+            <input type="radio" name="tag" id="tag-all" checked>
+            <label for="tag-all" class="button-red">all</label>
+            <input type="radio" name="tag" id="tag-graphic">
+            <label for="tag-graphic" class="button-red">graphic</label>
+            <input type="radio" name="tag" id="tag-illustration">
+            <label for="tag-illustration" class="button-red">illustration</label>
+            <input type="radio" name="tag" id="tag-motion">
+            <label for="tag-motion" class="button-red">motion</label>
+            <br>
+            <a href="work-01.html" class="tag-graphic"><img src="/img/thumb-01.jpg" alt="portfolio work-01"></a>
+            <a href="work-02.html" class="tag-graphic"><img src="/img/thumb-02.jpg" alt="portfolio work-02"></a>
+            <a href="work-03.html" class="tag-graphic"><img src="/img/thumb-03.jpg" alt="portfolio work-03"></a>
+            <a href="work-04.html" class="tag-graphic"><img src="/img/thumb-04.jpg" alt="portfolio work-04"></a>
+            <a href="work-05.html" class="tag-illustration"><img src="/img/thumb-05.jpg" alt="portfolio work-05"></a>
+            <a href="work-06.html" class="tag-illustration"><img src="/img/thumb-06.jpg" alt="portfolio work-06"></a>
+            <a href="work-07.html" class="tag-illustration"><img src="/img/thumb-07.jpg" alt="portfolio work-07"></a>
+            <a href="work-08.html" class="tag-illustration"><img src="/img/thumb-08.jpg" alt="portfolio work-08"></a>
+            <a href="work-09.html" class="tag-motion"><img src="/img/thumb-09.jpg" alt="portfolio work-09"></a>
+            <a href="work-10.html" class="tag-motion"><img src="/img/thumb-10.jpg" alt="portfolio work-10"></a>
+            <a href="work-11.html" class="tag-motion"><img src="/img/thumb-11.jpg" alt="portfolio work-11"></a>
+            <a href="work-12.html" class="tag-motion"><img src="/img/thumb-12.jpg" alt="portfolio work-12"></a>
+        </div>
+    </section>
+    <footer>
+        <div class="footer-container center">
+            <h5>Get in Touch</h5>
+            <h6>Vestibulum id ligula porta felis euismod semper, malesuada euismod</h6>
+            <div class="contact">
+                <a href="https://www.google.com.ua/maps/place/%D0%A3%D0%BB%D0%B8%D1%86%D0%B0+%D0%BF%D1%83%D1%88%D0%BA%D0%B8%D0%BD%D0%B0+%D0%B4%D0%BE%D0%BC+%D0%BA%D0%BE%D0%BB%D0%BE%D1%82%D1%83%D1%88%D0%BA%D0%B8%D0%BD%D0%B0/@55.8190893,37.4957068,17z/data=!3m1!4b1!4m2!3m1!1s0x46b5483b949ea495:0xc4ebcbe568aabe28?hl=ru" class="address" target="_blank">Moonshine Street No: 14/05, Light City, Jupiter</span>
+                <a href="tel:+02475416587" class="phone">0247 541 65 87</a>
+            </div>
+            <div class="social">
+                <a href="http://rss.example.com" class="social-icon rss" target="_blank"></a>
+                <a href="http://www.facebook.com" class="social-icon facebook" target="_blank"></a>
+                <a href="http://twitter.com" class="social-icon twitter" target="_blank"></a>
+                <a href="http://dribbble.com" class="social-icon dribbble" target="_blank"></a>
+                <a href="http://printerest.com" class="social-icon printerest" target="_blank"></a>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="copyright-container center">&#64; 2013 Webpaint. All Rights Reserved.</div>
+        </div>
+    </footer>
+</body>
+</html> ]
 
 jobs: .ad-author, .ad-city {
     font-style: italic;
@@ -278,117 +388,7 @@ $(".second-level").on("click", function(event){
         }
     });
 });
-      with: <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Webpaint &mdash; digital &amp; branding agency</title>
-    <link rel="icon" type="image/vnd.microsoft.icon" href="/img/favicon.ico">
-    <link rel="stylesheet" href="/css/custom-fonts.css">
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/css/adaptive.css">
-</head>
-<body>
-    <header>
-        <div class="header-container center">
-            <a href="/home.html" class="logo"></a>
-            <nav class="menu">
-                <div class="menu-item active"><a href="home.html">home</a></div>
-                <div class="menu-item"><a href="portfolio.html">portfolio</a></div>
-                <div class="menu-item"><a href="about.html">about</a></div>
-                <div class="menu-item"><a href="blog.html">blog</a></div>
-                <div class="menu-item"><a href="contact.html">contact</a></div>
-            </nav>
-        </div>
-    </header>
-    <section class="slider">
-        <div class="center">
-            <h1>We are <strong>Webpaint</strong></h1>
-            <h2><em>digital &amp; branding</em> agency based in Jupiter and we would love to turn ideas into beautiful things</h2>
-            <a href="#portfolio" class="button-red button-big">see portfolio</a>
-        </div>
-    </section>
-    <section class="services">
-        <div class="services-container center">
-            <div class="service-item">
-                <div class="service-icon service-icon-1"></div>
-                <div class="service-name">Consectetur</div>
-                <div class="service-description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iusto, quod expedita tempora placeat sequi dolor architecto.
-                </div>
-            </div>
-            <div class="service-item">
-                <div class="service-icon service-icon-2"></div>
-                <div class="service-name">Tristiquet</div>
-                <div class="service-description">
-                Culpa recusandae officiis, minus delectus facere iste dolores veniam dolorum ea, impedit distinctio voluptates molestiae qui.
-                </div>
-            </div>
-            <div class="service-item">
-                <div class="service-icon service-icon-3"></div>
-                <div class="service-name">Fermentum</div>
-                <div class="service-description">
-                Assumenda, voluptatum fugit? Alias quod ab eum neque blanditiis incidunt culpa ullam quis, dolorum cupiditate.
-                </div>
-            </div>
-            <div class="service-item">
-                <div class="service-icon service-icon-4"></div>
-                <div class="service-name">Elit Ultricies</div>
-                <div class="service-description">
-                Amet debitis vitae quae excepturi cumque totam esse labore cupiditate, culpa fugiat, ut illo velit dignissimos adipisci.
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="portfolio" class="portfolio">
-        <div class="center clearfix">
-            <h3>Our Featured Works</h3>
-            <h4>Sed sequi, maxime nisi consequuntur, illum asperiores repudiandae.</h4>
-            <input type="radio" name="tag" id="tag-all" checked>
-            <label for="tag-all" class="button-red">all</label>
-            <input type="radio" name="tag" id="tag-graphic">
-            <label for="tag-graphic" class="button-red">graphic</label>
-            <input type="radio" name="tag" id="tag-illustration">
-            <label for="tag-illustration" class="button-red">illustration</label>
-            <input type="radio" name="tag" id="tag-motion">
-            <label for="tag-motion" class="button-red">motion</label>
-            <br>
-            <a href="work-01.html" class="tag-graphic"><img src="/img/thumb-01.jpg" alt="portfolio work-01"></a>
-            <a href="work-02.html" class="tag-graphic"><img src="/img/thumb-02.jpg" alt="portfolio work-02"></a>
-            <a href="work-03.html" class="tag-graphic"><img src="/img/thumb-03.jpg" alt="portfolio work-03"></a>
-            <a href="work-04.html" class="tag-graphic"><img src="/img/thumb-04.jpg" alt="portfolio work-04"></a>
-            <a href="work-05.html" class="tag-illustration"><img src="/img/thumb-05.jpg" alt="portfolio work-05"></a>
-            <a href="work-06.html" class="tag-illustration"><img src="/img/thumb-06.jpg" alt="portfolio work-06"></a>
-            <a href="work-07.html" class="tag-illustration"><img src="/img/thumb-07.jpg" alt="portfolio work-07"></a>
-            <a href="work-08.html" class="tag-illustration"><img src="/img/thumb-08.jpg" alt="portfolio work-08"></a>
-            <a href="work-09.html" class="tag-motion"><img src="/img/thumb-09.jpg" alt="portfolio work-09"></a>
-            <a href="work-10.html" class="tag-motion"><img src="/img/thumb-10.jpg" alt="portfolio work-10"></a>
-            <a href="work-11.html" class="tag-motion"><img src="/img/thumb-11.jpg" alt="portfolio work-11"></a>
-            <a href="work-12.html" class="tag-motion"><img src="/img/thumb-12.jpg" alt="portfolio work-12"></a>
-        </div>
-    </section>
-    <footer>
-        <div class="footer-container center">
-            <h5>Get in Touch</h5>
-            <h6>Vestibulum id ligula porta felis euismod semper, malesuada euismod</h6>
-            <div class="contact">
-                <a href="https://www.google.com.ua/maps/place/%D0%A3%D0%BB%D0%B8%D1%86%D0%B0+%D0%BF%D1%83%D1%88%D0%BA%D0%B8%D0%BD%D0%B0+%D0%B4%D0%BE%D0%BC+%D0%BA%D0%BE%D0%BB%D0%BE%D1%82%D1%83%D1%88%D0%BA%D0%B8%D0%BD%D0%B0/@55.8190893,37.4957068,17z/data=!3m1!4b1!4m2!3m1!1s0x46b5483b949ea495:0xc4ebcbe568aabe28?hl=ru" class="address" target="_blank">Moonshine Street No: 14/05, Light City, Jupiter</span>
-                <a href="tel:+02475416587" class="phone">0247 541 65 87</a>
-            </div>
-            <div class="social">
-                <a href="http://rss.example.com" class="social-icon rss" target="_blank"></a>
-                <a href="http://www.facebook.com" class="social-icon facebook" target="_blank"></a>
-                <a href="http://twitter.com" class="social-icon twitter" target="_blank"></a>
-                <a href="http://dribbble.com" class="social-icon dribbble" target="_blank"></a>
-                <a href="http://printerest.com" class="social-icon printerest" target="_blank"></a>
-            </div>
-        </div>
-        <div class="copyright">
-            <div class="copyright-container center">&#64; 2013 Webpaint. All Rights Reserved.</div>
-        </div>
-    </footer>
-</body>
-</html>
+      with:
         python-version: '3.10'
     - name: Add conda to system path
       run: |
