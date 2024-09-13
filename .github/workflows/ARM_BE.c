c222c9d252880dc5255721804ebdb44891a870a1
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-09-14 00:06:49 +0500

    Update ARM_BE.c

--- .github/workflows/ARM_BE.c
+++ .github/workflows/ARM_BE.c
@@ -9232,4 +9232,33 @@ void Fifo_Flush(struct FIFO_struct *pFifo)
 } }}
       # Execute tests defined by the CMake configuration. Note that --build-config is needed because the default Windows generator is a multi-config generator (Visual Studio generator).
       # See https://cmake.org/cmake/help/latest/manual/ctest.1.html for more detail
-      run: ctest --build-config ${{ matrix.build_type }}
+      run: ctest --build-config ${{ import requests
+from email.utils import parseaddr
+
+def Check(account):
+        headers = {
+                'User-Agent':'Pwnage-Checker-For-Python',
+                "Accept": "application/vnd.haveibeenpwned.v2+json"
+        }
+        r = requests.get("https://haveibeenpwned.com/api/v2/breachedaccount/{0}".format(account),headers=headers)
+        names = {}
+        if not r.status_code == 200:
+                return False
+        json = r.json()
+        for i in range(len(json)):
+                names[json[i]["Name"]] = json[i]["BreachDate"]
+        return names
+
+a = input("-> ")
+liste = []
+a = a.replace("\n"," ")
+for i in a.split(","):
+        r = parseaddr(i)
+        if r[1] != "":
+                liste.append(r)
+for n in liste:
+        print("Checking ", str(n[0]))
+        che = Check(str(n[1]))
+        if che != False:
+                print(" > Results for ",str(n[1]),": \n",che,"\n")
+ }}
