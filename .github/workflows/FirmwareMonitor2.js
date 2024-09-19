9706bc445068503a8ab2ec0a24ac1fd34d12598b
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-09-20 00:23:48 +0500

    Update FirmwareMonitor2.js

--- .github/workflows/FirmwareMonitor2.js
+++ .github/workflows/FirmwareMonitor2.js
@@ -1,6 +1,295 @@
 name: Rust
 
-on: <?php
+on:  #Wed Apr 10 15:27:10 PDT 2013
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-2.2.1-all.zip
+      push: <?xml version='1.0' encoding='utf-8'?>
+<resources>
+    <array name="qt_sources">
+        <item>https://download.qt-project.org/ministro/android/qt5/qt-5.4</item>
+    </array>
+
+    <!-- The following is handled automatically by the deployment tool. It should
+         not be edited manually. -->
+
+    <array name="bundled_libs">
+        <!-- %%INSERT_EXTRA_LIBS%% -->
+    </array>
+
+     <array name="qt_libs">
+         <!-- %%INSERT_QT_LIBS%% -->
+     </array>
+
+    <array name="bundled_in_lib">
+        <!-- %%INSERT_BUNDLED_IN_LIB%% -->
+    </array>
+    <array name="bundled_in_assets">
+        <!-- %%INSERT_BUNDLED_IN_ASSETS%% -->
+    </array>
+
+</resources>
+    branches: [ "main" ]
+  pull_request: package labs.mavlink.VideoReceiverApp;
+
+/* Copyright 2013 Google Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * Project home page: http://code.google.com/p/usb-serial-for-android/
+ */
+///////////////////////////////////////////////////////////////////////////////////////////
+//  Written by: Mike Goza April 2014
+//
+//  These routines interface with the Android USB Host devices for serial port communication.
+//  The code uses the usb-serial-for-android software library.  The QGCActivity class is the
+//  interface to the C++ routines through jni calls.  Do not change the functions without also
+//  changing the corresponding calls in the C++ routines or you will break the interface.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.widget.Toast;
+import android.util.Log;
+import android.os.PowerManager;
+import android.os.Bundle;
+import android.app.PendingIntent;
+import android.view.WindowManager;
+import android.os.Bundle;
+import android.bluetooth.BluetoothDevice;
+
+import org.qtproject.qt5.android.bindings.QtActivity;
+import org.qtproject.qt5.android.bindings.QtApplication;
+
+public class QGLSinkActivity extends QtActivity
+{
+    public native void nativeInit();
+
+    // QGLSinkActivity singleton
+    public QGLSinkActivity() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        nativeInit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onInit(int status) {
+    }
+
+    public void jniOnLoad() {
+        nativeInit();
+    }
+}
+  branches: [ "main" ]
+
+env: /*
+ * Copyright (C) 2012, Collabora Ltd.
+ *   
+ *
+ * Copyright (C) 2015, Collabora Ltd.
+ *   
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+package org.freedesktop.gstreamer.androidmedia;
+
+import android.hardware.Camera;
+
+public class GstAhcCallback implements Camera.PreviewCallback,
+                                       Camera.ErrorCallback,
+                                       Camera.AutoFocusCallback {
+    public long mUserData;
+    public long mCallback;
+
+    public static native void gst_ah_camera_on_preview_frame(byte[] data, Camera camera,
+                                                             long callback, long user_data);
+    public static native void gst_ah_camera_on_error(int error, Camera camera,
+                                                     long callback, long user_data);
+    public static native void gst_ah_camera_on_auto_focus(boolean success, Camera camera,
+                                                             long callback, long user_data);
+
+    public GstAhcCallback(long callback, long user_data) {
+        mCallback = callback;
+        mUserData = user_data;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        gst_ah_camera_on_preview_frame(data, camera, mCallback, mUserData);
+    }
+
+    @Override
+    public void onError(int error, Camera camera) {
+        gst_ah_camera_on_error(error, camera, mCallback, mUserData);
+    }
+
+    @Override
+    public void onAutoFocus(boolean success, Camera camera) {
+        gst_ah_camera_on_auto_focus(success, camera, mCallback, mUserData);
+    }
+}
+  CARGO_TERM_COLOR: /*
+ * Copyright (C) 2016 SurroundIO
+ *   >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+package org.freedesktop.gstreamer.androidmedia;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class GstAhsCallback implements SensorEventListener {
+    public long mUserData;
+    public long mSensorCallback;
+    public long mAccuracyCallback;
+
+    public static native void gst_ah_sensor_on_sensor_changed(SensorEvent event,
+                                                              long callback, long user_data);
+    public static native void gst_ah_sensor_on_accuracy_changed(Sensor sensor, int accuracy,
+                                                                long callback, long user_data);
+
+    public GstAhsCallback(long sensor_callback,
+        long accuracy_callback, long user_data) {
+        mSensorCallback = sensor_callback;
+        mAccuracyCallback = accuracy_callback;
+        mUserData = user_data;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+      gst_ah_sensor_on_sensor_changed(event, mSensorCallback, mUserData);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+      gst_ah_sensor_on_accuracy_changed(sensor, accuracy,
+          mAccuracyCallback, mUserData);
+    }
+}
+
+jobs: /*
+ * Copyright (C) 2015, Collabora Ltd.
+ *   >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+package org.freedesktop.gstreamer.androidmedia;
+
+import android.graphics.SurfaceTexture;
+import android.graphics.SurfaceTexture.OnFrameAvailableListener;
+
+public class GstAmcOnFrameAvailableListener implements OnFrameAvailableListener
+{
+    private long context = 0;
+
+    public synchronized void onFrameAvailable (SurfaceTexture surfaceTexture) {
+        native_onFrameAvailable(context, surfaceTexture);
+    }
+
+    public synchronized long getContext () {
+        return context;
+    }
+
+    public synchronized void setContext (long c) {
+        context = c;
+    }
+
+    private native void native_onFrameAvailable (long context, SurfaceTexture surfaceTexture);
+}
+  build: QMainWindow State: 000000ff00000000fd00000004000000000000022d0000032cfc020000000efb0000001200530065006c0065006300740069006f006e00000001e10000009b0000006300fffffffb0000001e0054006f006f006c002000500072006f007000650072007400690065007302000001ed000001df00000185000000a3fb000000120056006900650077007300200054006f006f02000001df000002110000018500000122fb000000200054006f006f006c002000500072006f0070006500720074006900650073003203000002880000011d000002210000017afb000000100044006900730070006c006100790073010000004200000210000000dc00fffffffb0000002000730065006c0065006300740069006f006e00200062007500660066006500720200000138000000aa0000023a00000294fb00000014005700690064006500530074006500720065006f02000000e6000000d2000003ee0000030bfb0000000c004b0069006e0065006300740200000186000001060000030c00000261fb0000001800660072006f006e0074005f00630061006d006500720061010000038e000000160000000000000000fb00000016006200610063006b005f00630061006d00650072006101000003a4000000160000000000000000fb0000000c00430061006d0065007200610100000258000001160000001800fffffffb0000001e004d006f00740069006f006e00200050006c0061006e006e0069006e00670000000175000001e00000000000000000fb00000024005200760069007a00560069007300750061006c0054006f006f006c0073004700750069000000031f0000009b0000004800fffffffb0000000c00430061006d00650072006101000002f0000000ca0000000000000000000000010000010f00000392fc0200000003fb0000001e0054006f006f006c002000500072006f00700065007200740069006500730100000041000000780000000000000000fc00000028000003920000000000fffffffa000000000100000002fb0000000c00430061006d0065007200610000000000ffffffff0000000000000000fb0000000a0056006900650077007300000006300000010f0000010f00fffffffb0000001200530065006c0065006300740069006f006e010000025a000000b200000000000000000000000200000490000000a9fc0100000001fb0000000a00560069006500770073030000004e00000080000002e100000197000000030000073c00000042fc0100000002fb0000000800540069006d006501000000000000073c0000027000fffffffb0000000800540069006d00650100000000000004500000000000000000000005090000032c00000004000000040000000800000008fc0000000100000002000000010000000a0054006f006f006c00730100000000ffffffff0000000000000000
+
+
+    runs-on: <?php
 
 namespace VisitCounter\Db;
 
@@ -8,7 +297,8 @@ interface DbAdapterInterface
 {
     public function save(array $data);
 }
-  push: <?php
+
+    steps: <?php
 
 namespace VisitCounter\Db;
 
@@ -48,24 +338,21 @@ class PdoAdapter implements DbAdapterInterface
         }
     }
 }
-    branches: [ "main" ]
-  pull_request: <?php
+    - uses: <?php
 
 namespace VisitCounter\Exception;
 
 class DbException extends \Exception
 {
 }
-    branches: [ "main" ]
-
-env: <?php
+    - name: <?php
 
 namespace VisitCounter\Exception;
 
 class RedisException extends \Exception
 {
 }
-  CARGO_TERM_COLOR: <?php
+      run: <?php
 
 namespace VisitCounter\Redis;
 
@@ -79,8 +366,7 @@ interface RedisAdapterInterface
     public function hincrby($hashName, $field, $count = 1);
     public function hmget($hashName, array $fields);
 }
-
-jobs: <?php
+    - name: <?php
 
 namespace VisitCounter\Redis;
 
@@ -176,220 +462,4 @@ class RediskaAdapter implements RedisAdapterInterface
         return array_combine($fields, $result);
     }
 }
-  build: <?php
-
-class Post
-{
-    public $id;
-    public $title;
-    public $message;
-    public $visits;
-}
-
-    runs-on: <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-    <h1><?php echo $post->title;?></h1>
-    <p><?php echo $post->message;?></p>
-    <div class="counter">Unique visitors: <?php echo $totalVisits;?></div>
-    <form action="" method="POST">
-        <input type="submit" value="Save visits">
-    </form>
-    <p class="message"><?php echo $message;?></p>
-</body>
-</html>
-
-    steps: body {
-    width: 400px;
-    margin: auto;
-}
-.counter {
-    border: 1px solid red;
-    padding: 4px;
-    float: left;
-}
-input {
-    padding: 4px;
-    float: left;
-    margin: 0 20px 20px;
-}
-.message {
-    clear: both;
-    font-style: italic;
-    color: 555;
-}
-    - uses: <?php
-
-require "../vendor/autoload.php";
-
-$config = parse_ini_file('../config.ini');
-
-$options = array(
-    'servers' => array(
-       array('host' => $config['redisHost'], 'port' => $config['redisPort']),
-    )
-);
-
-$rediska = new Rediska($options);
-$rediskaAdapter = new \VisitCounter\Redis\RediskaAdapter($rediska);
-$vc = new \VisitCounter\VisitCounter($rediskaAdapter);
-$pageID = '1';
-$userIP = $_SERVER['REMOTE_ADDR'];
-$vc->countVisit($pageID, $userIP);
-
-$dbh = new PDO(
-    $config['dbDsn'],
-    $config['dbUser'],
-    $config['dbPass'], 
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
-
-$sql = "SELECT id, title, message, visits FROM posts WHERE id=:id";
-$sth = $dbh->prepare($sql);
-$sth->bindValue(':id', $pageID, PDO::PARAM_INT);
-$sth->execute();
-$sth->setFetchMode(PDO::FETCH_CLASS, 'Post');
-$post = $sth->fetch();
-
-$recentVisits = $vc->getDeltaVisits([$pageID])[$pageID];
-$savedVisits = $post->visits;
-$totalVisits = intval($recentVisits) + intval($savedVisits);
-
-$message = '';
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $pdoAdapter = new \VisitCounter\Db\PdoAdapter($dbh, 'posts', 'visits');
-    $vc->moveToDb($pdoAdapter);
-    $message = 'Visits from redis was successfully transfered to database.';
-}
-
-require "../template/template.php";
-    - name: {
-    "repositories": [
-        {
-            "type": "vcs",
-            "url": "https://github.com/nsdvw/visit-counter"
-        }
-    ],
-    "require": {
-        "geometria-lab/rediska": "^0.5.10",
-        "nsdvw/visit-counter": "dev-master"
-    },
-    "autoload": {
-        "files": ["app/Post.php"]
-    }
-}
-      run: {
-    "_readme": [
-        "This file locks the dependencies of your project to a known state",
-        "Read more about it at https://getcomposer.org/doc/01-basic-usage.md#composer-lock-the-lock-file",
-        "This file is @generated automatically"
-    ],
-    "hash": "826e2457fc05380510fd1d3db56917a5",
-    "content-hash": "42055bc9b6d4d6c6cbfeb2a7c8f74ca5",
-    "packages": [
-        {
-            "name": "geometria-lab/rediska",
-            "version": "v0.5.10",
-            "source": {
-                "type": "git",
-                "url": "https://github.com/Shumkov/Rediska.git",
-                "reference": "5b33ba2f1526f9ad30864b42617e34ac370aeae0"
-            },
-            "dist": {
-                "type": "zip",
-                "url": "https://api.github.com/repos/Shumkov/Rediska/zipball/5b33ba2f1526f9ad30864b42617e34ac370aeae0",
-                "reference": "5b33ba2f1526f9ad30864b42617e34ac370aeae0",
-                "shasum": ""
-            },
-            "require-dev": {
-                "phpunit/phpunit": ">=3.7.11",
-                "zendframework/zendframework1": ">=1.12.1"
-            },
-            "type": "library",
-            "autoload": {
-                "psr-0": {
-                    "Rediska": [
-                        "library/",
-                        "tests/library/"
-                    ]
-                }
-            },
-            "notification-url": "https://packagist.org/downloads/",
-            "authors": [
-                {
-                    
-                }
-            ],
-            "description": "Full-featured PHP client for key-value database Redis",
-            "keywords": [
-                "cache",
-                "redis",
-                "sessions",
-                "zend"
-            ],
-            "time": "2014-08-12 15:02:35"
-        },
-        {
-            "name": "nsdvw/visit-counter",
-            "version": "dev-master",
-            "source": {
-                "type": "git",
-                "url": "https://github.com/nsdvw/visit-counter.git",
-                "reference": "7dc1ff516c1bb0855720a690d693e932ac2139a4"
-            },
-            "dist": {
-                "type": "zip",
-                "url": "https://api.github.com/repos/nsdvw/visit-counter/zipball/7dc1ff516c1bb0855720a690d693e932ac2139a4",
-                "reference": "7dc1ff516c1bb0855720a690d693e932ac2139a4",
-                "shasum": ""
-            },
-            "type": "library",
-            "autoload": {
-                "psr-4": {
-                    "VisitCounter\\": "./"
-                }
-            },
-            "description": "Simple visitor counter on redis, cross-client and extensible",
-            "support": {
-                "source": "https://github.com/nsdvw/visit-counter/tree/master",
-                "issues": "https://github.com/nsdvw/visit-counter/issues"
-            },
-            "time": "2015-12-01 23:44:58"
-        }
-    ],
-    "packages-dev": [],
-    "aliases": [],
-    "minimum-stability": "stable",
-    "stability-flags": {
-        "nsdvw/visit-counter": 20
-    },
-    "prefer-stable": false,
-    "prefer-lowest": false,
-    "platform": [],
-    "platform-dev": []
-}
-    - name: redisHost="127.0.0.1"
-redisPort="6379"
-dbDsn="mysql:host=127.0.0.1;dbname=visit_counter"
-dbUser="root"
-dbPass=""
-      run: CREATE DATABASE visit_counter;
-USE visit_counter;
-CREATE TABLE posts (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
-    visits SMALLINT NOT NULL DEFAULT 0
-);
-
-INSERT INTO posts VALUES (
-    1,
-    'HELLO WORLD',
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut nesciunt exercitationem eius nobis. Est in magnam velit laborum fugit alias pariatur quo eius tempora quisquam, facere tempore reiciendis distinctio culpa!',
-    0
-);
+      run: cargo test --verbose
