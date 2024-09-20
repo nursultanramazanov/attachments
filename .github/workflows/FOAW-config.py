0cc5e076452d36da00127359e7ae7dd4ed2271e4
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-09-20 12:40:31 +0500

    Update and rename b-config.py to FOAW-config.py

--- .github/workflows/FOAW-config.py
+++ .github/workflows/FOAW-config.py
@@ -1,34 +1,36 @@
 name: C/C++ CI
 
-on: import requests
-from email.utils import parseaddr
+on: Copyright (c) 2024, ramazanovnursultan 
+All rights reserved.
 
-def Check(account):
-        headers = {
-                'User-Agent':'Pwnage-Checker-For-Python',
-                "Accept": "application/vnd.haveibeenpwned.v2+json"
-        }
-        r = requests.get("https://haveibeenpwned.com/api/v2/breachedaccount/{0}".format(account),headers=headers)
-        names = {}
-        if not r.status_code == 200:
-                return False
-        json = r.json()
-        for i in range(len(json)):
-                names[json[i]["Name"]] = json[i]["BreachDate"]
-        return names
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
-a = input("-> ")
-liste = []
-a = a.replace("\n"," ")
-for i in a.split(","):
-        r = parseaddr(i)
-        if r[1] != "":
-                liste.append(r)
-for n in liste:
-        print("Checking ", str(n[0]))
-        che = Check(str(n[1]))
-        if che != False:
-                print(" > Results for ",str(n[1]),": \n",che,"\n")
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of McGill University, nor the Input Devices and
+      Music Interaction Lab, nor the names of its contributors may be
+      used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   push: cp vendor/twbs/bootstrap/dist/css/bootstrap.min.css web/vendor-css/bootstrap.min.css;
 cp vendor/twbs/bootstrap/dist/js/bootstrap.min.js web/vendor-js/bootstrap.min.js;
 cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.eot web/fonts/glyphicons-halflings-regular.eot;
