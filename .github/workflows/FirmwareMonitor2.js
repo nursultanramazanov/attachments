da85e362f10a78813d5a2e1ceb79cf185520844f
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-09-20 11:51:55 +0500

    ESP32NFC.js

--- .github/workflows/FirmwareMonitor2.js
+++ .github/workflows/FirmwareMonitor2.js
@@ -1,295 +1,169 @@
 name: Rust
 
-on:  #Wed Apr 10 15:27:10 PDT 2013
-distributionBase=GRADLE_USER_HOME
-distributionPath=wrapper/dists
-zipStoreBase=GRADLE_USER_HOME
-zipStorePath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-2.2.1-all.zip
-      push: <?xml version='1.0' encoding='utf-8'?>
-<resources>
-    <array name="qt_sources">
-        <item>https://download.qt-project.org/ministro/android/qt5/qt-5.4</item>
-    </array>
+on: -----BEGIN CERTIFICATE-----
+MIIDRTCCAi2gAwIBAgIGMTM2OTY5MA0GCSqGSIb3DQEBCwUAMF4xEDAOBgNVBAMT
+B1Rlc3QgQ0ExCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYD
+VQQHEw1TYW4gRnJhbmNpc2NvMRAwDgYDVQQKEwdUZXN0IENBMB4XDTIyMDMwNTE5
+MDU0NloXDTIzMDMwNTE5MDU0NlowEzERMA8GA1UEAxMIZGMubG9jYWwwggEiMA0G
+CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC65kPnRp6v4pSoc1celQy1veAXKKma
+uUU+TfHWWBw/NOReepxsWxDgl+g9N8ISMrbIZB2tnmqaCXAV/E4GcggHlihm5Mci
+JYznsREtQ0q1dA2VGHhtX28b9V9TeBhiF6ZXIqfKkclCl7xCgUCJQ0JV6jCS84GI
+pSi4DD9XPdfjdUa9fU+5Cs/XfkCC1bqiWlpPBticD4uydFdB6N8vJlo41uKLfPUi
+bduqvQWAkDrBLVT2MSJxZEOaM5T6b3zilwZBeWwfwo/X3KmGze7rHL9ItQM7vUPH
+k5jnLZYfTTztHCUOfN058Mm8tJNzMXBYsJJS53+cRsrcYiWhk1MRwotpAgMBAAGj
+VDBSMAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsG
+AQUFBwMBBggrBgEFBQcDAjATBgNVHREEDDAKgghkYy5sb2NhbDANBgkqhkiG9w0B
+AQsFAAOCAQEAfPv5vzoJDFF9OPJ34tzK6zdcJVb5sRWUdAs8PIxBqrv0wSyW8iA6
+9nJI3Fw9/hmF/ipLZlmDFYvKvYdMgU1i2P0mhmz377Yj/XgO/6z4izY9L1NliS8z
+9AioA7wKf5vEri8f4w3w1nwKxWPcENvSv9Bm73uGk6cOYzXnxJMx4QxnxAx276tA
+SV8AvrCAoKK9CUxp+VWAVp2HYBRt/sFT7NA020vCbS7I/cbYDBbs+FpnDrI9cBAr
+UtTlThfHbH2XdSjV5TTeg+NqqfZfz0kxHloOuX53C5W7T/brmWGjvQkcq3b2+CYY
+VxPTjjpduhkjhvJs6p19d87F0SMkP3ONGA==
+-----END CERTIFICATE-----
 
-    <!-- The following is handled automatically by the deployment tool. It should
-         not be edited manually. -->
-
-    <array name="bundled_libs">
-        <!-- %%INSERT_EXTRA_LIBS%% -->
-    </array>
-
-     <array name="qt_libs">
-         <!-- %%INSERT_QT_LIBS%% -->
-     </array>
-
-    <array name="bundled_in_lib">
-        <!-- %%INSERT_BUNDLED_IN_LIB%% -->
-    </array>
-    <array name="bundled_in_assets">
-        <!-- %%INSERT_BUNDLED_IN_ASSETS%% -->
-    </array>
-
-</resources>
+-----BEGIN CERTIFICATE-----
+MIIDXjCCAkagAwIBAgIFOTA4MTUwDQYJKoZIhvcNAQELBQAwXjEQMA4GA1UEAxMH
+VGVzdCBDQTELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNV
+BAcTDVNhbiBGcmFuY2lzY28xEDAOBgNVBAoTB1Rlc3QgQ0EwHhcNMjIwMzA0MTUz
+OTUxWhcNMjMwMzA0MTUzOTUxWjBeMRAwDgYDVQQDEwdUZXN0IENBMQswCQYDVQQG
+EwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNj
+bzEQMA4GA1UEChMHVGVzdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAMjvly5N/iieO1YM97pB+K5eJQ5Jxu+LRePgEDAEwysxHgmP5g0xdaw1tZNN
+dw8Z5/emvNUFecCKjU0IgPTjlfdLgHOkg16+fEOOj5FlN7I4Lc9teAhdBtHPZr+n
+ok0XNdEulI5ijFeZdIyO4gBvwKfwwCY71XLxNiNLsQvuDUxabDyaFppNJliYdAuw
+yh1jEpd+sFPWtsEK56chcchKu4yKJZWaeSvD7bJAJCdaSdnknSUYc9S5RoR+sAu4
+DpSl9tAO8bKRgyo9ApnwqWS3RM1yYY9z6GJGauXH1BHXpMEoc5h2/nTaI9ripqHy
+GaQwMmnGtcTdvbL5wTDl6mkHQw0CAwEAAaMjMCEwDwYDVR0TAQH/BAUwAwEB/zAO
+BgNVHQ8BAf8EBAMCAgQwDQYJKoZIhvcNAQELBQADggEBAFfziw0Pjdmo3pgu900J
+Pw6H/yCqEvQIEa7HrFowVbutr9feIRPXKEI91mSlkFXhX8axM0mu/KHkUrhr1ApY
+wW1hH1+dOZfiPSvhi3GGkwsD2M43xMOYfNo0COpBvogLu44xJapT7i908Krocg4M
+w2zAse4o+0m6fGD99tyc5PnbIJ09M/zeHyxwPtnbW9hIvum/CdoLcylZD2jYd9d3
+9quxbjrj0oHynGRjV20esQuRqNHrqC/QlNnEVomeL62OrPIbrhRIFbG77zrteNxB
+gZTmrCIAlaVm7caGpk+e7pKPb5C7OyWWPLYeR5h9e+hNta0+ht80/jYvUS5nw5Ge
+0ZE=
+-----END CERTIFICATE-----
+  push: -----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAuuZD50aer+KUqHNXHpUMtb3gFyipmrlFPk3x1lgcPzTkXnqc
+bFsQ4JfoPTfCEjK2yGQdrZ5qmglwFfxOBnIIB5YoZuTHIiWM57ERLUNKtXQNlRh4
+bV9vG/VfU3gYYhemVyKnypHJQpe8QoFAiUNCVeowkvOBiKUouAw/Vz3X43VGvX1P
+uQrP135AgtW6olpaTwbYnA+LsnRXQejfLyZaONbii3z1Im3bqr0FgJA6wS1U9jEi
+cWRDmjOU+m984pcGQXlsH8KP19yphs3u6xy/SLUDO71Dx5OY5y2WH0087RwlDnzd
+OfDJvLSTczFwWLCSUud/nEbK3GIloZNTEcKLaQIDAQABAoIBAHnK4gcNYw101rDq
+8KOyN2UbhbMxwUxdXT0bp/Oo2UdrHUJgHh0SPu5Mr39tHV+kgq4VZ62jJQ6aBdud
+3e6fUpI3DL74aLfcxnSZU3sudbnHFa7KEngbj5kljegjw6fwh2oS4ErGM+PcY0bn
+WXnZRZodEWhBnHDCR8Wf3Yrpdwz2Wz/mtg+c8CMjTV4WG7OZvYMRwIm8Sapqbws+
+YzBzv1YSNgxg1iOuvCXkqvL26ENiBuk+ibbIz3QOYEqCRQ0dqXNCzd0s7anK0aIG
+3RqsmCXF4vEbb3ie1lf6YE7fn0ld8/1LDjVBhmpLoNpbFNTubuoDD3vVYS42W8wj
+3VQO74ECgYEA49E0Oan9m2cS89JC6/6nsRVvbjWBTiZn0/wW9uLMNK/IvQ1C41rc
+MpGk/RzMQHxz0LaWFDcVSng3GdA6LDdDe56JWIlrCvFCDDLL38Ss8TebsliQJP7+
+uGXvjxT70aX6ErCZURD/L3xZdpu3Q3p/LkUpAYsAriYMuS1AtdBHwDECgYEA0gU6
+aAFg5jNdPWVXe4eeJ1vfGST9m1L+hO3Zu8FcpcRp8u7u1+zWJZWmt4Knux9WFjoR
++jd87ihuYiOkH31KpH8aJmXJ/OctMQLwDSms7JsQc7n8pxB5ye5pq6e6yuAknHf+
+cOnwXRqxeN3nUB7bs+hT38/McGhWkxKouFaxKLkCgYEAtDmefTfT34rNXYIrWxMc
+imUQwDkn0DFl1jgJ2spVDiE6RwUks0QACylZQrvzgdhyrtyjId7pwv4TaSJnwu79
+PeCPezFLM17lB01wJ9gpkLKTQ6X4cqwfyrUprSHGjXYmvX5j69PsbR6Mj95kPse7
+sVkZNZVHtn13d08rcCoTeSECgYAt8CTRYRPP5oZLiqIzC/wFAexvtI6gbevsKZaJ
+ogbg33CJScgGhPBmcjBwTWzg2j7bR8Js57w037UdVExu8tnj10ViBrvCzNIuuUTG
+rEn9ywEzPwhLhqgNClAk0uBhxdtcOkb2bcU8qD0PggI5nTZTSMsn1m0SXVAqhVdQ
+Uu4XSQKBgBn7UlZnQN0FOh0yGdnq6Pz8GFI9WxYaNjAu8JF+lsfSOMbPYJwTjxhb
+clrVPaB8FWy9XLat8ewSfvPIgEIbpyyYGEDL9UM4BzYnNmosQ4PN0xhvMtw4Ilp7
+NgMFbaN5R8IeVJNwZnrCXzRJXx8sycl+Fb2wzyiq3cbw/mMtfSCq
+-----END RSA PRIVATE KEY-----
     branches: [ "main" ]
-  pull_request: package labs.mavlink.VideoReceiverApp;
-
-/* Copyright 2013 Google Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * Project home page: http://code.google.com/p/usb-serial-for-android/
- */
-///////////////////////////////////////////////////////////////////////////////////////////
-//  Written by: Mike Goza April 2014
-//
-//  These routines interface with the Android USB Host devices for serial port communication.
-//  The code uses the usb-serial-for-android software library.  The QGCActivity class is the
-//  interface to the C++ routines through jni calls.  Do not change the functions without also
-//  changing the corresponding calls in the C++ routines or you will break the interface.
-//
-////////////////////////////////////////////////////////////////////////////////////////////
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.io.IOException;
-
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbAccessory;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
-import android.widget.Toast;
-import android.util.Log;
-import android.os.PowerManager;
-import android.os.Bundle;
-import android.app.PendingIntent;
-import android.view.WindowManager;
-import android.os.Bundle;
-import android.bluetooth.BluetoothDevice;
-
-import org.qtproject.qt5.android.bindings.QtActivity;
-import org.qtproject.qt5.android.bindings.QtApplication;
-
-public class QGLSinkActivity extends QtActivity
-{
-    public native void nativeInit();
-
-    // QGLSinkActivity singleton
-    public QGLSinkActivity() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        nativeInit();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    public void onInit(int status) {
-    }
-
-    public void jniOnLoad() {
-        nativeInit();
-    }
-}
-  branches: [ "main" ]
-
-env: /*
- * Copyright (C) 2012, Collabora Ltd.
- *   
- *
- * Copyright (C) 2015, Collabora Ltd.
- *   
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
- *
- */
-
-package org.freedesktop.gstreamer.androidmedia;
-
-import android.hardware.Camera;
-
-public class GstAhcCallback implements Camera.PreviewCallback,
-                                       Camera.ErrorCallback,
-                                       Camera.AutoFocusCallback {
-    public long mUserData;
-    public long mCallback;
-
-    public static native void gst_ah_camera_on_preview_frame(byte[] data, Camera camera,
-                                                             long callback, long user_data);
-    public static native void gst_ah_camera_on_error(int error, Camera camera,
-                                                     long callback, long user_data);
-    public static native void gst_ah_camera_on_auto_focus(boolean success, Camera camera,
-                                                             long callback, long user_data);
-
-    public GstAhcCallback(long callback, long user_data) {
-        mCallback = callback;
-        mUserData = user_data;
-    }
-
-    @Override
-    public void onPreviewFrame(byte[] data, Camera camera) {
-        gst_ah_camera_on_preview_frame(data, camera, mCallback, mUserData);
-    }
-
-    @Override
-    public void onError(int error, Camera camera) {
-        gst_ah_camera_on_error(error, camera, mCallback, mUserData);
-    }
-
-    @Override
-    public void onAutoFocus(boolean success, Camera camera) {
-        gst_ah_camera_on_auto_focus(success, camera, mCallback, mUserData);
-    }
-}
-  CARGO_TERM_COLOR: /*
- * Copyright (C) 2016 SurroundIO
- *   >
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- */
-
-package org.freedesktop.gstreamer.androidmedia;
-
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-
-public class GstAhsCallback implements SensorEventListener {
-    public long mUserData;
-    public long mSensorCallback;
-    public long mAccuracyCallback;
-
-    public static native void gst_ah_sensor_on_sensor_changed(SensorEvent event,
-                                                              long callback, long user_data);
-    public static native void gst_ah_sensor_on_accuracy_changed(Sensor sensor, int accuracy,
-                                                                long callback, long user_data);
-
-    public GstAhsCallback(long sensor_callback,
-        long accuracy_callback, long user_data) {
-        mSensorCallback = sensor_callback;
-        mAccuracyCallback = accuracy_callback;
-        mUserData = user_data;
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-      gst_ah_sensor_on_sensor_changed(event, mSensorCallback, mUserData);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-      gst_ah_sensor_on_accuracy_changed(sensor, accuracy,
-          mAccuracyCallback, mUserData);
-    }
-}
-
-jobs: /*
- * Copyright (C) 2015, Collabora Ltd.
- *   >
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
- *
- */
-
-package org.freedesktop.gstreamer.androidmedia;
-
-import android.graphics.SurfaceTexture;
-import android.graphics.SurfaceTexture.OnFrameAvailableListener;
-
-public class GstAmcOnFrameAvailableListener implements OnFrameAvailableListener
-{
-    private long context = 0;
-
-    public synchronized void onFrameAvailable (SurfaceTexture surfaceTexture) {
-        native_onFrameAvailable(context, surfaceTexture);
-    }
-
-    public synchronized long getContext () {
-        return context;
-    }
-
-    public synchronized void setContext (long c) {
-        context = c;
-    }
-
-    private native void native_onFrameAvailable (long context, SurfaceTexture surfaceTexture);
-}
-  build: QMainWindow State: 000000ff00000000fd00000004000000000000022d0000032cfc020000000efb0000001200530065006c0065006300740069006f006e00000001e10000009b0000006300fffffffb0000001e0054006f006f006c002000500072006f007000650072007400690065007302000001ed000001df00000185000000a3fb000000120056006900650077007300200054006f006f02000001df000002110000018500000122fb000000200054006f006f006c002000500072006f0070006500720074006900650073003203000002880000011d000002210000017afb000000100044006900730070006c006100790073010000004200000210000000dc00fffffffb0000002000730065006c0065006300740069006f006e00200062007500660066006500720200000138000000aa0000023a00000294fb00000014005700690064006500530074006500720065006f02000000e6000000d2000003ee0000030bfb0000000c004b0069006e0065006300740200000186000001060000030c00000261fb0000001800660072006f006e0074005f00630061006d006500720061010000038e000000160000000000000000fb00000016006200610063006b005f00630061006d00650072006101000003a4000000160000000000000000fb0000000c00430061006d0065007200610100000258000001160000001800fffffffb0000001e004d006f00740069006f006e00200050006c0061006e006e0069006e00670000000175000001e00000000000000000fb00000024005200760069007a00560069007300750061006c0054006f006f006c0073004700750069000000031f0000009b0000004800fffffffb0000000c00430061006d00650072006101000002f0000000ca0000000000000000000000010000010f00000392fc0200000003fb0000001e0054006f006f006c002000500072006f00700065007200740069006500730100000041000000780000000000000000fc00000028000003920000000000fffffffa000000000100000002fb0000000c00430061006d0065007200610000000000ffffffff0000000000000000fb0000000a0056006900650077007300000006300000010f0000010f00fffffffb0000001200530065006c0065006300740069006f006e010000025a000000b200000000000000000000000200000490000000a9fc0100000001fb0000000a00560069006500770073030000004e00000080000002e100000197000000030000073c00000042fc0100000002fb0000000800540069006d006501000000000000073c0000027000fffffffb0000000800540069006d00650100000000000004500000000000000000000005090000032c00000004000000040000000800000008fc0000000100000002000000010000000a0054006f006f006c00730100000000ffffffff0000000000000000
+  pull_request: import hashlib
+import random
+from typing import List
+import requests
+import json
+import math
 
 
-    runs-on: <?php
+URL = "https://dc.local/token/"
+PSK = "1234"
+
+NUM_TOKENS = 200
+TOKEN_LENGTH = 32
+BATCH_SIZE = 50
+
+def getRandomToken():
+    token = ""
+    for _ in range(TOKEN_LENGTH):
+        byte = random.randint(0, 255)
+        token += f'{byte:02x}'
+    return token
+
+
+def removeDuplicates(_list):
+    _set = set(_list)
+    return list(_set)
+
+
+def sort(_list):
+    return sorted(_list)
+
+def makeRequestBody(tokens):
+    body =  '{\n'
+    body += '\t"tokens": [\n'
+    for token in tokens:
+        body += '\t\t{\n'
+        body += f'\t\t\t"toPut": "{token}"\n'
+        body += '\t\t},\n'
+    if body[-2] == ',':
+        body = body[:-2] + '\n'
+    body += '\t]\n'
+    body += '}\n'
+    return body
+
+
+def sendBatched(tokens: List[str]):
+    while len(tokens) > 0:
+        print(len(tokens))
+        data = makeRequestBody(tokens[:BATCH_SIZE])
+        tokens = tokens[BATCH_SIZE:]      
+        resp = requests.request(
+            "PUT",
+            URL,
+            headers={
+                "psk": PSK
+            },
+            data=data,
+            verify=False
+        )
+        print(f"Resp code: {resp.status_code}")
+        print(f"Resp body: {resp.content}")        
+
+
+tokensList = [getRandomToken() for _ in range(NUM_TOKENS)]
+tokensUnique = removeDuplicates(tokensList)
+tokensSorted = sort(tokensUnique)
+
+tokensStr = ""
+for token in tokensSorted:
+    tokensStr += token
+
+hash = hashlib.sha256(tokensStr.encode());
+print(hash.hexdigest())
+
+print(f"Unique: {len(tokensUnique)}")
+
+sendBatched(tokensList)
+
+
+resp = requests.request(
+    "GET",
+    URL,
+    headers={
+        "psk": PSK
+    },
+    verify=False
+)
+
+dcHash = str(resp.json()['hash'])
+print(dcHash)
+print(hash.hexdigest() == dcHash)
+    branches: [ "main" ]
+
+env: <?php
 
 namespace VisitCounter\Db;
 
@@ -298,7 +172,7 @@ interface DbAdapterInterface
     public function save(array $data);
 }
 
-    steps: <?php
+  CARGO_TERM_COLOR: <?php
 
 namespace VisitCounter\Db;
 
@@ -338,21 +212,23 @@ class PdoAdapter implements DbAdapterInterface
         }
     }
 }
-    - uses: <?php
+
+jobs: <?php
 
 namespace VisitCounter\Exception;
 
 class DbException extends \Exception
 {
 }
-    - name: <?php
+  build: <?php
 
 namespace VisitCounter\Exception;
 
 class RedisException extends \Exception
 {
 }
-      run: <?php
+
+    runs-on: <?php
 
 namespace VisitCounter\Redis;
 
@@ -366,7 +242,8 @@ interface RedisAdapterInterface
     public function hincrby($hashName, $field, $count = 1);
     public function hmget($hashName, array $fields);
 }
-    - name: <?php
+
+    steps: <?php
 
 namespace VisitCounter\Redis;
 
@@ -462,4 +339,8 @@ class RediskaAdapter implements RedisAdapterInterface
         return array_combine($fields, $result);
     }
 }
+    - uses: actions/checkout@v4
+    - name: Build
+      run: cargo build --verbose
+    - name: Run tests
       run: cargo test --verbose
