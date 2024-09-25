26450185833c1ee157f42a825f2a2895ed02dd08
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-09-25 20:37:49 +0500

    Update origami1980.h

--- .github/workflows/origami1980.h
+++ .github/workflows/origami1980.h
@@ -23,117 +23,66 @@
 
 name: "Mayhem for API"
 
-on: import requests
-import base64
+on: resume_from_checkpoint_path: null
+result_path: "result"
+model_path: null
+dataset_paths: ["path/to/train.jsonl"]
+tokenizer: "dataset/tokenizer.json"
+exp_name: "nougat"
+train_batch_sizes: [1]
+num_workers: 8
+val_batch_sizes: [1]
+val_batches: 1
+input_size: [896, 672]
+max_length: 4096
+max_position_embeddings: 4096
+accumulate_grad_batches: 3
+window_size: 7
+patch_size: 4
+embed_dim: 128
+hidden_dimension: 1024
+num_heads: [4, 8, 16, 32]
+encoder_layer: [2, 2, 14, 2]
+decoder_layer: 10
+align_long_axis: False
+num_nodes: 1
+seed: 25
+lr: 5e-5
+min_lr: 7.5e-6
+lr_step: 16
+gamma: 0.9996
+warmup_steps: 250
+num_training_samples_per_epoch: 10000
+max_epochs: 30
+max_steps: -1
+val_check_interval: null
+check_val_every_n_epoch: 1
+gradient_clip_val: 0.5
+verbose: False
+  push: FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+# replace CUDA version to your CUDA version.
+# You can check your CUDA version with below.
+# nvcc -V
 
-a = requests.post("https://www.hackthebox.eu/api/invite/generate")
-b = a.json()
-print(b)
-if b["success"] == 1:
-        password = b["data"]["code"]
-        print(base64.b64decode(password).decode())
-else:
-        print("Failed")
-  push: import socket
-import platform
-from requests import get
-from lxml import etree
-import os
-from subprocess import Popen
+RUN apt-get update
+RUN apt-get install -y python3
+RUN apt-get -y install python3-pip git
+RUN pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# replace CUDA version to your CUDA version.
 
+RUN mkdir workspace
+WORKDIR /workspace
 
-class Main():
-        mycostatus = False
-        ip = ''
-        if socket.gethostbyname(socket.gethostname()) != '127.0.0.1':
-                local_ip = socket.gethostbyname(socket.gethostname())
-        def __init__(self):
-                try:
-                        moninstance = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        moninstance.connect(("8.8.8.8", 80)) # Google DNS
-                except socket.error:
-                        self.mycostatus = False
-                else:
-                        self.ip = get('https://api.ipify.org').text
+RUN pip3 install fastapi uvicorn[standard] fsspec[http]==2023.1.0
+RUN git clone https://github.com/facebookresearch/nougat.git
+WORKDIR /workspace/nougat
 
-                        self.mycostatus = True
-                finally:
-                        moninstance.close()
-        def IpDetails(self, target=ip):
-                if self.mycostatus == True:
-                        details = get('http://ip-api.com/xml/{}'.format(str(target))).text
-                        nveaufichierxml = open("resultatip.xml", 'w')
-                        nveaufichierxml.write(str(details))
-                        nveaufichierxml.close()
-                        tree = etree.parse("resultatip.xml")
-                        for a in tree.xpath("/query/country"):
-                                country = a.text
-                        for b in tree.xpath("/query/countryCode"):
-                                countrycode = b.text
-                        for c in tree.xpath("/query/region"):
-                                region = c.text
-                        for d in tree.xpath("/query/regionName"):
-                                regionName = d.text
-                        for e in tree.xpath("/query/city"):
-                                city = e.text
-                        for f in tree.xpath("/query/zip"):
-                                zipcode = f.text
-                        for g in tree.xpath("/query/lat"):
-                                latitude = g.text
-                        for h in tree.xpath("/query/lon"):
-                                longitude = h.text
-                        for i in tree.xpath("/query/timezone"):
-                                timezone = i.text
-                        for j in tree.xpath("/query/isp"):
-                                ispname = j.text
-                        for k in tree.xpath("/query/org"):
-                                organization = k.text
-                        for l in tree.xpath("/query/as"):
-                                As = l.text
-                        for m in tree.xpath("/query/query"):
-                                cible = m.text
-                        print("   0000-----------------{}-----------------0000".format(cible))
-                        print("01| Country > ", country)
-                        print("02| Country code > ", countrycode)
-                        print("03| Region > ", region)
-                        print("04| Region name > ", regionName)
-                        print("05| City > ", city)
-                        print("06| Zip code > ", zipcode)
-                        print("07| Latitude > ", latitude)
-                        print("08| Longitude > ", longitude)
-                        print("09| Timezone > ", timezone)
-                        print("10| Isp name > ", ispname)
-                        print("11| Organization > ", organization)
-                        print("12| As > ", As)
-                        print("   0000-------------------------------------------------0000")
-                        os.remove("resultatip.xml")#FileNotFoundError
-        def PublicIpAddress(self):
-                if self.mycostatus == True:
-                        self.ip = get('https://api.ipify.org').text
-                        return self.ip
-        def MyPcDetails(self):
-                pc_details = platform.uname()
-                print("|________________________________________________________________|")
-                print("")
-                if self.mycostatus == True:
-                        print("Internet access: OK")
-                        print("Local ip: ", self.local_ip)
-                        print("External ip: ", self.ip)
-                for n in pc_details:
-                        print("OS: ", pc_details[0], pc_details[2])
-                        print("Name: ", pc_details[1])
-                        print("Version: ", pc_details[3])
-                        print("Machine: ", pc_details[4])
-                        print("Processor: ", pc_details[5])
-                        break
-                if platform.system() == 'Linux':
-                        distribu = platform.linux_distribution()
-                        for o in distribu:
-                                print("Distrib: ", distribu[0], distribu[1])
-                                print("Id: ", distribu[2])
-                                break
-                print("")
-                print("|________________________________________________________________|")
+RUN python3 setup.py install
+
+EXPOSE 8503
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8503"]
+# Run this using 'docker run -it -d -p <YOUR PORT>:8503 --gpus all <IMAGE NAME>                             
     branches: [ "main" ]
   pull_request: RewriteEngine On
 
