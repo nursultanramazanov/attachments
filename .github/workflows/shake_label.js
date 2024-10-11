485824dfada8027db4b9fdc2c97f87df126aff88
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-10-12 01:53:49 +0500

    Update shake_label.js

--- .github/workflows/shake_label.js
+++ .github/workflows/shake_label.js
@@ -368,12 +368,363 @@ impl Terminal {
   label: U+202C ⁯⁣U+2068⁫ U+202D ⁡⁬U+2069⁤⁡ U+2069 U+202D U+2069 U+2068 U+202D⁡⁬ U+2069 U+2069 U+202D ⁥U+2066 ⁡⁬U+2069 ⁤⁡U+2069 U+202D ⁡⁬U+2069 ⁤⁡U+202D U+2066 U+202D U+2068 U+202D U+202E ⁮U+202D U+202D U+202E
   
 
-    runs-on: ubuntu-latest
-    permissions:
-      contents: read
-      pull-requests: write
+    runs-on: //
+//  ActivityObserver.h
+//  Clock Signal
+//
+//  Created by on 07/05/2018.
+//  Copyright 2018 . All rights reserved.
+//
 
-    steps:
-    - uses: actions/labeler@v4
-      with:
-        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace Activity {
+
+/*!
+        Provides a purely virtual base class for anybody that wants to receive notifications of
+        'activity': any feedback from an emulated system which a user could perceive other than
+        through the machine's native audio and video outputs.
+
+        So: status LEDs, drive activity, etc. A receiver may choose to make appropriate noises
+        and/or to show or unshow status indicators.
+*/
+class Observer {
+        public:
+                virtual ~Observer() = default;
+
+                /// Provides hints as to the sort of information presented on an LED.
+                enum LEDPresentation: uint8_t {
+                        /// This LED informs the user of some sort of persistent state, e.g. scroll lock.
+                        /// If this flag is absent then the LED describes an ephemeral state, such as media access.
+                        Persistent = (1 << 0),
+                };
+
+                /// Announces to the receiver that there is an LED of name @c name.
+                virtual void register_led([[maybe_unused]] const std::string &name, [[maybe_unused]] uint8_t presentation = 0) {}
+
+                /// Announces to the receiver that there is a drive of name @c name.
+                ///
+                /// If a drive has the same name as an LED, that LED goes with this drive.
+                virtual void register_drive([[maybe_unused]] const std::string &name) {}
+
+                /// Informs the receiver of the new state of the LED with name @c name.
+                virtual void set_led_status([[maybe_unused]] const std::string &name, [[maybe_unused]] bool lit) {}
+
+                enum class DriveEvent {
+                        StepNormal,
+                        StepBelowZero,
+                        StepBeyondMaximum
+                };
+
+                /// Informs the receiver that the named event just occurred for the drive with name @c name.
+                virtual void announce_drive_event([[maybe_unused]] const std::string &name, [[maybe_unused]] DriveEvent event) {}
+
+                /// Informs the receiver of the motor-on status of the drive with name @c name.
+                virtual void set_drive_motor_status([[maybe_unused]] const std::string &name, [[maybe_unused]] bool is_on) {}
+};
+
+}
+    permissions: //
+//  ActivitySource.h
+//  Clock Signal
+//
+//  Created by on 07/05/2018.
+//  Copyright 2018. All rights reserved.
+//
+
+#pragma once
+
+#include "Observer.hpp"
+
+namespace Activity {
+
+class Source {
+        public:
+                virtual void set_activity_observer(Observer *observer) = 0;
+};
+
+}
+      contents: <?php
+namespace Tests\TestHubBundle\Controller;
+
+use Tests\TestHubBundle\TestCase;
+
+class TestControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(0, $crawler->filter('.test-container')->count());
+    }
+
+    public function testPreface()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/test/1/preface');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('.test-container')->count());
+        $this->assertEquals(1, $crawler->filter('.test-start-button')->count());
+
+        $client->request('GET', '/test/0/preface');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+}
+      pull-requests: <?php
+namespace Tests\TestHubBundle\Helper;
+
+use TestHubBundle\Helper\StringGenerator;
+use Tests\TestHubBundle\TestCase;
+
+class StringGeneratorTest extends TestCase
+{
+    /**
+     * What to test:
+     *
+     * return value type
+     * return value length (between 10 and 100 chars)
+     * return value uniqueness (at least 2 must differ)
+     */
+    public function testGenerateString()
+    {
+        $string = StringGenerator::generateString();
+        $this->assertInternalType('string', $string);
+
+        $iterations = 5;
+        $randoms = [];
+        for ($i = 0; $i < $iterations; $i++) {
+            $randoms[] = StringGenerator::generateString();
+        }
+        $this->assertTrue(count(array_unique($randoms)) > 1);
+
+        $string = StringGenerator::generateString();
+        $this->assertGreaterThan(10, mb_strlen($string));
+        $this->assertLessThan(100, mb_strlen($string));
+    }
+
+}
+
+    steps: <?php
+namespace Tests\TestHubBundle\Service;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use TestHubBundle\Service\Calculator;
+use Tests\TestHubBundle\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    protected $em;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $container = self::getApplication()->getKernel()->getContainer();
+        $this->em = $container->get('doctrine')->getManager();
+    }
+
+    public function testCountCorrectAnswers()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+
+        $calculator = new Calculator();
+        $correctAnswersCount = $calculator->countCorrectAnswers($attempt);
+        $this->assertInternalType('integer', $correctAnswersCount);
+        $this->assertEquals(2, $correctAnswersCount);
+    }
+
+    public function testCalculateMark()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+
+        $calculator = new Calculator();
+        $mark = $calculator->calculateMark($attempt);
+        $this->assertInternalType('integer', $mark);
+        $this->assertEquals(15, $mark);
+    }
+
+    public function testCalculateMaxMark()
+    {
+        $test = $this->em->find('TestHubBundle:Test', 1);
+
+        $calculator = new Calculator();
+        $maxMark = $calculator->calculateMaxMark($test);
+        $this->assertInternalType('integer', $maxMark);
+        $this->assertEquals(30, $maxMark);
+    }
+}
+    - uses: <?php
+namespace Tests\TestHubBundle\Service;
+
+use Tests\TestHubBundle\TestCase;
+
+class TestServiceTest extends TestCase
+{
+    private $em;
+    private $testService;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $container = $this->getApplication()->getKernel()->getContainer();
+        $this->testService = $container->get('test_service');
+        $this->em = $container->get('doctrine')->getManager();
+    }
+
+    public function testGetUnansweredCount()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+
+        $count = $this->testService->getUnansweredCount($attempt);
+        $this->assertInternalType('integer', $count);
+        $this->assertEquals(1, $count);
+    }
+
+    public function testQuestionAlreadyHasAnswer()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+        $expectHasAnswer = $this->em->find('TestHubBundle:Question', 4);
+        $expectNoAnswer = $this->em->find('TestHubBundle:Question', 1);
+
+        $true = $this->testService->questionAlreadyHasAnswer($attempt, $expectHasAnswer);
+        $false = $this->testService->questionAlreadyHasAnswer($attempt, $expectNoAnswer);
+
+        $this->assertTrue($true);
+        $this->assertFalse($false);
+    }
+
+    public function testGetFirstUnanswered()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+        $question = $this->testService->getFirstUnanswered($attempt);
+
+        $this->assertInstanceOf('TestHubBundle\Entity\Question', $question);
+        $this->assertEquals(1, $question->getId());
+    }
+
+    public function testGetNextUnansweredNumber()
+    {
+        $attempt = $this->em->find('TestHubBundle:Attempt', 1);
+        $number = 1;
+
+        $nextNumber = $this->testService->getNextUnansweredNumber($attempt, $number);
+
+        $this->assertEquals(4, $nextNumber);
+    }
+
+    public function testGetQuestionsCount()
+    {
+        $test = $this->em->find('TestHubBundle:Test', 1);
+
+        $count = $this->testService->getQuestionsCount($test);
+        $this->assertEquals(4, $count);
+    }
+}
+      with: <?php
+namespace Tests\TestHubBundle\Twig;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use TestHubBundle\Twig\AppExtension;
+
+class AppExtensionTest extends WebTestCase
+{
+    private $ext;
+
+    protected function setUp()
+    {
+        $this->ext = new AppExtension();
+    }
+
+    public function testWordCaseFilter()
+    {
+        $wordForms = ['день', 'дня', 'дней'];
+
+        $this->assertEquals('1 день', $this->ext->wordCase(1, $wordForms));
+        $this->assertEquals('3 дня', $this->ext->wordCase(3, $wordForms));
+        $this->assertEquals('5 дней', $this->ext->wordCase(5, $wordForms));
+        $this->assertEquals('17 дней', $this->ext->wordCase(17, $wordForms));
+        $this->assertEquals('22 дня', $this->ext->wordCase(22, $wordForms));
+        $this->assertEquals('113 дней', $this->ext->wordCase(113, $wordForms));
+        $this->assertEquals('269 дней', $this->ext->wordCase(269, $wordForms));
+
+        $wordForms = ['вопрос', 'вопроса', 'вопросов'];
+
+        $this->assertEquals('1 вопрос', $this->ext->wordCase(1, $wordForms));
+        $this->assertEquals('3 вопроса', $this->ext->wordCase(3, $wordForms));
+        $this->assertEquals('5 вопросов', $this->ext->wordCase(5, $wordForms));
+        $this->assertEquals('17 вопросов', $this->ext->wordCase(17, $wordForms));
+        $this->assertEquals('22 вопроса', $this->ext->wordCase(22, $wordForms));
+        $this->assertEquals('113 вопросов', $this->ext->wordCase(113, $wordForms));
+        $this->assertEquals('269 вопросов', $this->ext->wordCase(269, $wordForms));
+
+        $wordForms = ['минута', 'минуты', 'минут'];
+
+        $this->assertEquals('1 минута', $this->ext->wordCase(1, $wordForms));
+        $this->assertEquals('3 минуты', $this->ext->wordCase(3, $wordForms));
+        $this->assertEquals('5 минут', $this->ext->wordCase(5, $wordForms));
+        $this->assertEquals('17 минут', $this->ext->wordCase(17, $wordForms));
+        $this->assertEquals('22 минуты', $this->ext->wordCase(22, $wordForms));
+        $this->assertEquals('113 минут', $this->ext->wordCase(113, $wordForms));
+        $this->assertEquals('269 минут', $this->ext->wordCase(269, $wordForms));
+    }
+
+    public function testFormatTimeLeftFilter()
+    {
+        /*
+         * 1 day 4 hours 18 minutes 23 seconds
+         */
+        $time = 3600 * 24 + 4 * 3600 + 18 * 60 + 23;
+        $this->assertEquals('1 день 04:18:23', $this->ext->formatTimeLeftFilter($time));
+    }
+
+    public function testPercentage()
+    {
+        $this->assertEquals('66%', $this->ext->percentage(2, 3));
+        $this->assertEquals('0%', $this->ext->percentage(0, 3));
+        $this->assertEquals('100%', $this->ext->percentage(3, 3));
+    }
+}
+        repo-token: <?php
+namespace Tests\TestHubBundle;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\StringInput;
+
+class TestCase extends WebTestCase
+{
+    protected static $application;
+
+    protected function setUp()
+    {
+        self::runCommand('doctrine:fixtures:load -e test --no-interaction --purge-with-truncate');
+    }
+
+    protected static function runCommand($command)
+    {
+        $command = sprintf('%s --quiet', $command);
+
+        return self::getApplication()->run(new StringInput($command));
+    }
+
+    protected static function getApplication()
+    {
+        if (null === self::$application) {
+            $client = static::createClient();
+
+            self::$application = new Application($client->getKernel());
+            self::$application->setAutoExit(false);
+        }
+
+        return self::$application;
+    }
+}
