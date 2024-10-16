41492b59944dc9df069037b7fe1501eb7edb05d2
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-10-16 18:48:18 +0500

    Update ARM_BE.c

--- .github/workflows/ARM_BE.c
+++ .github/workflows/ARM_BE.c
@@ -114,35 +114,10 @@ class Main():
 		print("")
 		print("|________________________________________________________________|")
     branches: [ "main" ]
-  pull_request: import requests
-from email.utils import parseaddr
-
-def Check(account):
-	headers = {
-		'User-Agent':'Pwnage-Checker-For-Python',
-		"Accept": "application/vnd.haveibeenpwned.v2+json"
-	}
-	r = requests.get("https://haveibeenpwned.com/api/v2/breachedaccount/{0}".format(account),headers=headers)
-	names = {}
-	if not r.status_code == 200:
-		return False
-	json = r.json()
-	for i in range(len(json)):
-		names[json[i]["Name"]] = json[i]["BreachDate"]
-	return names
-
-a = input("-> ")
-liste = []
-a = a.replace("\n"," ")
-for i in a.split(","):
-	r = parseaddr(i)
-	if r[1] != "":
-		liste.append(r)
-for n in liste:
-	print("Checking ", str(n[0]))
-	che = Check(str(n[1]))
-	if che != False:
-		print(" > Results for ",str(n[1]),": \n",che,"\n")
+  pull_request: idf_component_register(SRCS main.c db_esp32_control.c globals.h sdkconfig.h msp_ltm_serial.c
+        msp_ltm_serial.h db_protocol.h http_server.c http_server.h db_esp32_comm.c
+        db_esp32_comm.h db_comm_protocol.h db_comm.c db_comm.h db_crc.c db_crc.h tcp_server.c tcp_server.h
+        INCLUDE_DIRS ".") 
     branches: [ "main" ]
 
 jobs: #
