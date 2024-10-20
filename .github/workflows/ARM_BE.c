1cc53c36b6079f200275b884e924b365b868ef72
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-10-20 11:41:42 +0500

    Update ARM_BE.c

--- .github/workflows/ARM_BE.c
+++ .github/workflows/ARM_BE.c
@@ -2,117 +2,59 @@
 # See: https://github.com/actions/starter-workflows/blob/main/ci/cmake-single-platform.yml
 name: CMake on multiple platforms
 
-on: import requests
-import base64
+on: [target.x86_64-pc-windows-msvc]
+rustflags = ["-C", "target-feature=+crt-static"]
 
-a = requests.post("https://www.hackthebox.eu/api/invite/generate")
-b = a.json()
-print(b)
-if b["success"] == 1:
-	password = b["data"]["code"]
-	print(base64.b64decode(password).decode())
-else:
-	print("Failed")
-  push: import socket
-import platform
-from requests import get
-from lxml import etree
-import os
-from subprocess import Popen
+[target.'cfg(all(windows, debug_assertions))']
+rustflags = [
+  "-C",
+  "target-feature=+crt-static",
+  "-C",
+  # increase the stack size to prevent overflowing the
+  # stack in debug when launching sub commands
+  "link-arg=/STACK:4194304",
+]
 
+[target.x86_64-apple-darwin]
+rustflags = [
+  "-C",
+  "link-args=-weak_framework Metal -weak_framework MetalPerformanceShaders -weak_framework QuartzCore -weak_framework CoreGraphics",
+]
 
-class Main():
-	mycostatus = False
-	ip = ''
-	if socket.gethostbyname(socket.gethostname()) != '127.0.0.1':
-		local_ip = socket.gethostbyname(socket.gethostname())
-	def __init__(self):
-		try:
-			moninstance = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			moninstance.connect(("8.8.8.8", 80)) # Google DNS
-		except socket.error:
-			self.mycostatus = False
-		else:
-			self.ip = get('https://api.ipify.org').text
-			
-			self.mycostatus = True
-		finally:
-			moninstance.close()
-	def IpDetails(self, target=ip):
-		if self.mycostatus == True:
-			details = get('http://ip-api.com/xml/{}'.format(str(target))).text
-			nveaufichierxml = open("resultatip.xml", 'w')
-			nveaufichierxml.write(str(details))
-			nveaufichierxml.close()
-			tree = etree.parse("resultatip.xml")
-			for a in tree.xpath("/query/country"):
-				country = a.text
-			for b in tree.xpath("/query/countryCode"):
-				countrycode = b.text
-			for c in tree.xpath("/query/region"):
-				region = c.text
-			for d in tree.xpath("/query/regionName"):
-				regionName = d.text
-			for e in tree.xpath("/query/city"):
-				city = e.text
-			for f in tree.xpath("/query/zip"):
-				zipcode = f.text
-			for g in tree.xpath("/query/lat"):
-				latitude = g.text
-			for h in tree.xpath("/query/lon"):
-				longitude = h.text
-			for i in tree.xpath("/query/timezone"):
-				timezone = i.text
-			for j in tree.xpath("/query/isp"):
-				ispname = j.text
-			for k in tree.xpath("/query/org"):
-				organization = k.text
-			for l in tree.xpath("/query/as"):
-				As = l.text
-			for m in tree.xpath("/query/query"):
-				cible = m.text
-			print("   0000-----------------{}-----------------0000".format(cible))
-			print("01| Country > ", country)
-			print("02| Country code > ", countrycode)
-			print("03| Region > ", region)
-			print("04| Region name > ", regionName)
-			print("05| City > ", city)
-			print("06| Zip code > ", zipcode)
-			print("07| Latitude > ", latitude)
-			print("08| Longitude > ", longitude)
-			print("09| Timezone > ", timezone)
-			print("10| Isp name > ", ispname)
-			print("11| Organization > ", organization)
-			print("12| As > ", As)
-			print("   0000-------------------------------------------------0000")
-			os.remove("resultatip.xml")#FileNotFoundError
-	def PublicIpAddress(self):
-		if self.mycostatus == True:
-			self.ip = get('https://api.ipify.org').text
-			return self.ip
-	def MyPcDetails(self):
-		pc_details = platform.uname()
-		print("|________________________________________________________________|")
-		print("")
-		if self.mycostatus == True:
-			print("Internet access: OK")
-			print("Local ip: ", self.local_ip)
-			print("External ip: ", self.ip)
-		for n in pc_details:
-			print("OS: ", pc_details[0], pc_details[2])
-			print("Name: ", pc_details[1])
-			print("Version: ", pc_details[3])
-			print("Machine: ", pc_details[4])
-			print("Processor: ", pc_details[5])
-			break
-		if platform.system() == 'Linux':
-			distribu = platform.linux_distribution()
-			for o in distribu:
-				print("Distrib: ", distribu[0], distribu[1])
-				print("Id: ", distribu[2])
-				break
-		print("")
-		print("|________________________________________________________________|")
+[target.aarch64-apple-darwin]
+rustflags = [
+  "-C",
+  "link-args=-fuse-ld=lld -weak_framework Metal -weak_framework MetalPerformanceShaders -weak_framework QuartzCore -weak_framework CoreGraphics",
+]
+
+[target.'cfg(all())']
+rustflags = [
+  "-D",
+  "clippy::all",
+  "-D",
+  "clippy::await_holding_refcell_ref",
+  "-D",
+  "clippy::missing_safety_doc",
+  "-D",
+  "clippy::undocumented_unsafe_blocks",
+  "--cfg",
+  "tokio_unstable",
+]
+  push: # To run a build using a local tree:
+#
+# 0. Check out these repositories as siblings:
+#
+#     - https://github.com/denoland/deno
+#     - https://github.com/denoland/deno_core
+#     - https://github.com/denoland/rusty_v8
+#
+# 1. From `deno`, run: cargo --config .cargo/local-build.toml build
+
+[patch.crates-io]
+deno_core = { path = "../deno_core/core" }
+deno_ops = { path = "../deno_core/ops" }
+serde_v8 = { path = "../deno_core/serde_v8" }
+v8 = { path = "../rusty_v8" }			
     branches: [ "main" ]
   pull_request: idf_component_register(SRCS main.c db_esp32_control.c globals.h sdkconfig.h msp_ltm_serial.c
         msp_ltm_serial.h db_protocol.h http_server.c http_server.h db_esp32_comm.c
