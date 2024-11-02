82283f78407e3dfd2256304d7ad5b847d77b0298
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2024-11-02 09:05:38 +0500

    Update ArmV12.py

--- .github/workflows/ArmV12.py
+++ .github/workflows/ArmV12.py
@@ -1587,247 +1587,115 @@ gdb_flash_program disable
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 openocd -f interface/olimex-arm-usb-ocd-h.cfg -f $DIR/px4fmu-v1-board.cfg 
-    - name: cp vendor/twbs/bootstrap/dist/css/bootstrap.min.css ../css/bootstrap.min.css;
-cp vendor/twbs/bootstrap/dist/js/bootstrap.min.js ../js/bootstrap.min.js;
-cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.eot ../fonts/glyphicons-halflings-regular.eot;
-cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.svg ../fonts/glyphicons-halflings-regular.svg;
-cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf ../fonts/glyphicons-halflings-regular.ttf;
-cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff ../fonts/glyphicons-halflings-regular.woff;
-cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2 ../fonts/glyphicons-halflings-regular.woff2;
+    - name: <?php
 
-cp vendor/select2/select2/select2.js ../js/select2.js;
-cp vendor/select2/select2/select2_locale_ru.js ../js/select2_locale_ru.js;
-cp vendor/select2/select2/select2.css ../css/select2.css;
-cp vendor/select2/select2/select2.png ../css/select2.png;
-cp vendor/select2/select2/select2-spinner.gif ../css/select2-spinner.gif;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 
-cp vendor/silverfire/select2-bootstrap3-css/select2-bootstrap.min.css ../css/select2-bootstrap.min.css; 
+require "../vendor/autoload.php";
+require "../config/settings.php";
+
+$devMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(
+    [__DIR__ . "/Model"],
+    $devMode
+);
+$config->addEntityNamespace('Memtext', 'Memtext\Model');
+
+$db = $settings['db'];
+$conn = [
+    'host' => $db['host'],
+    'dbname' => $db['dbname'],
+    'user' => $db['user'],
+    'pass' => $db['pass'],
+    'driver' => $db['driver'],
+];
+
+$em = EntityManager::create($conn, $config);         
       run: <?php
 
-class FakerCommand extends CConsoleCommand
-{
-    const PER_INSERT = 100; // how many rows should be inserted in one query
-    const PER_TRANSACTION = 100; // how many queries should contain one transaction
-    const PASSWORD = 'demo';
-    const PUBLISHED = 'published';
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+use Memtext\Mapper\SphinxMapper;
+use Memtext\Service\TextService;
+use Memtext\Service\LoginManager;
+use Memtext\Handler\NotFoundHandler;
+use Memtext\Helper\TextParser;
 
-    private $fakerData = array();
-    private $builder;
-    private $password;
+$container = $app->getContainer();
 
-    public function init()
-    {
-        $this->fakerData = new FakerData(self::PER_INSERT);
-        $this->builder = Yii::app()->db->getSchema()->getCommandBuilder();
-        $this->password = CPasswordHelper::hashPassword(self::PASSWORD);
+$container['csrf_token'] = $token;
+
+// obtain em from bootstrap.php
+$container['entityManager'] = $em;
+
+$container['sphinx_conn'] = function ($c) {
+    $dbalConfig = new Configuration();
+    $sphinx_config = $c['settings']['sphinx'];
+    $driver = $c['settings']['db']['driver'];
+    $connectionParams = [
+        'host' => $sphinx_config['host'],
+        'port' => $sphinx_config['port'],
+        'driver' => $driver,
+    ];
+    return DriverManager::getConnection(
+        $connectionParams,
+        $dbalConfig
+    );
+};
+
+$container['sphinxMapper'] = function ($c) {
+    return new SphinxMapper($c['sphinx_conn']);
+};
+
+$container['loginManager'] = function ($c) {
+    return new LoginManager(
+        $c['entityManager']->getRepository('Memtext:User'),
+        $c['csrf_token']
+    );
+};
+
+$container['purifier'] = function ($c) {
+    $config = \HTMLPurifier_Config::createDefault();
+    $settings = $c['settings']['purifier'];
+    foreach ($settings as $key => $value) {
+        $config->set($key, $value);
     }
+    return new \HTMLPurifier($settings);
+};
 
-    public function actionUser($cnt = 1000)
-    {
-        $table = 'user'; 
-        $insertCount = intval(floor($cnt/self::PER_INSERT));
-        $txn = Yii::app()->db->beginTransaction();
-        for ($i=0; $i < $insertCount; $i++) {
-            $this->progressMsg($i * self::PER_INSERT, $cnt);
-            $data = $this->collectUserData();
-            $this->multipleInsert($table, $data);
-            if ($i % self::PER_TRANSACTION == 0 and $i != 0) {
-                $txn->commit();
-                $txn = Yii::app()->db->beginTransaction();
-            }
-        }
-        $remainder = $cnt % self::PER_INSERT;
-        if ($remainder) {
-            $data = $this->collectUserData($remainder);
-            $this->multipleInsert($table, $data);
-        }
-        $txn->commit();
-    }
+$container['textParser'] = new TextParser;
 
-    public function actionAd($cnt=1000, $eav=false, $photos=false)
-    {
-        echo date('H:i:s') . "\n";
-        $insertCount = intval(floor($cnt/self::PER_INSERT));
-        $txn = Yii::app()->db->beginTransaction();
-        for ($i=0; $i < $insertCount; $i++) {
-            $this->progressMsg($i * self::PER_INSERT, $cnt);
-            $this->insertAd($eav, $photos);
-            if ($i % self::PER_TRANSACTION == 0 and $i != 0) {
-                $txn->commit();
-                $txn = Yii::app()->db->beginTransaction();
-            }
-        }
-        $remainder = $cnt % self::PER_INSERT;
-        if ($remainder) {
-            $this->insertAd($eav, $photos, $remainder);
-        }
-        $txn->commit();
-        echo date('H:i:s') . "\n";
-    }
+$container['textService'] = function ($c) {
+    return new TextService(
+        $c['textParser'],
+        $c['entityManager'],
+        $c['sphinxService']
+    );
+};
 
-    private function multipleInsert($table, $data)
-    {
-        $command = $this->builder->createMultipleInsertCommand($table, $data);
-        $command->execute();
-    }
+$container['sphinxService'] = function ($c) {
+    $sphinxConfig = $c['settings']['sphinx'];
+    return new \Memtext\Service\SphinxService(
+        $c['sphinxMapper'],
+        $sphinxConfig['indexName']
+    );
+};
 
-    private function eavMultipleInsert($table, $data)
-    {
-        $sql = "INSERT INTO {$table} (eav_attribute_id, entity_id, entity, value) VALUES ";
-        $i = 0;
-        foreach ($data as $row) {
-            if ($i == 0) {
-                $sql .= "(:a{$i},:e{$i},'ad',:v{$i})";
-            } else {
-                $sql .= ",(:a{$i},:e{$i},'ad',:v{$i})";
-            }
-            $params[":e{$i}"] = $row['entity_id'];
-            $params[":a{$i}"] = $row['eav_attribute_id'];
-            $params[":v{$i}"] = $row['value'];
-            $i++;
-        }
-        Yii::app()->db->createCommand($sql)->execute($params);
-    }
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('../templates');
+    $view->addExtension(new \Twig_Extensions_Extension_Text());
+    $view['loginManager'] = $c['loginManager'];
+    $view['csrf_token'] = $c['csrf_token'];
+    return $view;
+};
 
-    private function insertAd($eav, $photos, $cnt = self::PER_INSERT)
-    {
-        $table = 'ad';
-        $data = $this->collectAdData($cnt);
-        $this->multipleInsert($table, $data);
-        $this->attachEav($eav, $cnt);
-        $this->attachPhoto($photos, $cnt);
-    }
+$container['notFoundHandler'] = function ($c) {
+    return new NotFoundHandler(
+        $c['view'],
+        function (Request $request, Response $response) use ($c) {
+            return $c['response']->withStatus(404);
+        });
+};       
 
-    private function collectUserData($cnt = self::PER_INSERT)
-    {
-        $data = array();
-        for ($i=0; $i < $cnt; $i++) {
-            $data[] = array(
-                'email' => $this->fakerData->getEmail(),
-                // use 'demo' to login
-                'password' => $this->password,
-                'name' => $this->fakerData->getUserName(),
-                'phone' => $this->fakerData->getPhoneNumber(),
-            );
-        }
-        return $data;
-    }
-
-    private function collectAdData($cnt = self::PER_INSERT)
-    {
-        $data = array();
-        for ($i=0; $i < $cnt; $i++) {
-            $category = $this->fakerData->getCategory();
-            $data[] = array(
-                'title' => $this->fakerData->getTitle(),
-                'description' => $this->fakerData->getDescription(),
-                'author_id' => $this->fakerData->getAuthor(),
-                'city_id' => $this->fakerData->getCity(),
-                'category_id' => $category['id'],
-                'eav_set_id' => $category['set_id'],
-                'status' => self::PUBLISHED,
-            );
-        }
-        return $data;
-    }
-
-    private function attachEav($eav, $cnt = self::PER_INSERT)
-    {
-        if (!$eav) return;
-        $data = $this->collectEavData($cnt);
-        if (!empty($data['int'])) {
-            $this->eavMultipleInsert('eav_attribute_int', $data['int']);
-        }
-        if (!empty($data['varchar'])) {
-            $this->eavMultipleInsert('eav_attribute_varchar', $data['varchar']);
-        }
-    }
-
-    private function collectEavData($cnt)
-    {
-        $ads = $this->getAdsCommand($cnt)->queryAll();
-        foreach ($ads as $ad) {
-            $set = $this->fakerData->getSet($ad['eav_set_id']);
-            foreach ($set as $attr) {
-                if ($attr['data_type'] == 'IntDataType') {
-                    $value = $this->getEavIntValue($attr);
-                    $data['int'][] = array(
-                        'eav_attribute_id' => $attr['eav_attribute_id'],
-                        'entity_id' => $ad['id'],
-                        'entity' => 'ad',
-                        'value' => $value,
-                    );
-                } elseif ($attr['data_type'] == 'VarcharDataType') {
-                    $value = $this->getEavVarcharValue($attr);
-                    $data['varchar'][] = array(
-                        'eav_attribute_id' => $attr['eav_attribute_id'],
-                        'entity_id' => $ad['id'],
-                        'entity' => 'ad',
-                        'value' => $value,
-                    );
-                }
-            }
-        }
-        return $data;
-    }
-
-    private function attachPhoto($photos, $cnt = self::PER_INSERT)
-    {
-        if (!$photos) return;
-        $data = $this->collectPhotoData($cnt);
-        $this->multipleInsert('photo', $data);
-    }
-
-    private function collectPhotoData($cnt)
-    {
-        $data = array();
-        $ads = $this->getAdsCommand($cnt)->queryAll();
-        foreach ($ads as $ad) {
-            $photoCount = rand(0, 5);
-            for ($i=0; $i < $photoCount; $i++) {
-                $data[] = array(
-                    'name' => $this->fakerData->getPhotoName(),
-                    'ad_id' => $ad['id'],
-                );
-            }
-        }
-        return $data;
-    }
-
-    private function getEavIntValue($attr)
-    {
-        $rawData = unserialize($attr['data']);
-        $min = (isset($rawData['rules']['numerical']['min']))
-                ? intval($rawData['rules']['numerical']['min'])
-                : 0;
-        $max = (isset($rawData['rules']['numerical']['max']))
-                ? intval($rawData['rules']['numerical']['max'])
-                : 100000000;
-        if ($attr['name'] == 'price') {
-            $value = rand($min, intval($max/100));
-        } elseif ($attr['name'] == 'modelYear') {
-            $max = intval(date('Y'));
-            $value = rand($min, $max);
-        } else {
-            $value = rand($min, $max);
-        }
-        return $value;
-    }
-
-    private function getEavVarcharValue($attr)
-    {
-        $rawData = unserialize($attr['data']);
-        return $rawData['values'][array_rand($rawData['values'])];
-    }
-
-    private function getAdsCommand($cnt)
-    {
-        $sql = "SELECT id, eav_set_id FROM ad ORDER BY id DESC LIMIT $cnt";
-        return Yii::app()->db->createCommand($sql);
-    }
-
-    private function progressMsg($cur, $cnt)
-    {
-        echo round($cur * 100 / $cnt, 2) . "%\n";
-    }
-} 
+            
