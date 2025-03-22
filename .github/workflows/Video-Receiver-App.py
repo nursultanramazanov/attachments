5ae2e4d622244c71463bcede1cddb23cd3a030f2
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2025-03-22 23:18:07 +0500

    Update and rename  Video-Receiver-App.py

--- .github/workflows/Video-Receiver-App.py
+++ .github/workflows/Video-Receiver-App.py
@@ -439,76 +439,118 @@ public class GstAmcOnFrameAvailableListener implements OnFrameAvailableListener
     private native void native_onFrameAvailable (long context, SurfaceTexture surfaceTexture);
 } 
 
-env: -----BEGIN CERTIFICATE-----
-MIIDRTCCAi2gAwIBAgIGMTM2OTY5MA0GCSqGSIb3DQEBCwUAMF4xEDAOBgNVBAMT
-B1Rlc3QgQ0ExCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYD
-VQQHEw1TYW4gRnJhbmNpc2NvMRAwDgYDVQQKEwdUZXN0IENBMB4XDTIyMDMwNTE5
-MDU0NloXDTIzMDMwNTE5MDU0NlowEzERMA8GA1UEAxMIZGMubG9jYWwwggEiMA0G
-CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC65kPnRp6v4pSoc1celQy1veAXKKma
-uUU+TfHWWBw/NOReepxsWxDgl+g9N8ISMrbIZB2tnmqaCXAV/E4GcggHlihm5Mci
-JYznsREtQ0q1dA2VGHhtX28b9V9TeBhiF6ZXIqfKkclCl7xCgUCJQ0JV6jCS84GI
-pSi4DD9XPdfjdUa9fU+5Cs/XfkCC1bqiWlpPBticD4uydFdB6N8vJlo41uKLfPUi
-bduqvQWAkDrBLVT2MSJxZEOaM5T6b3zilwZBeWwfwo/X3KmGze7rHL9ItQM7vUPH
-k5jnLZYfTTztHCUOfN058Mm8tJNzMXBYsJJS53+cRsrcYiWhk1MRwotpAgMBAAGj
-VDBSMAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsG
-AQUFBwMBBggrBgEFBQcDAjATBgNVHREEDDAKgghkYy5sb2NhbDANBgkqhkiG9w0B
-AQsFAAOCAQEAfPv5vzoJDFF9OPJ34tzK6zdcJVb5sRWUdAs8PIxBqrv0wSyW8iA6
-9nJI3Fw9/hmF/ipLZlmDFYvKvYdMgU1i2P0mhmz377Yj/XgO/6z4izY9L1NliS8z
-9AioA7wKf5vEri8f4w3w1nwKxWPcENvSv9Bm73uGk6cOYzXnxJMx4QxnxAx276tA
-SV8AvrCAoKK9CUxp+VWAVp2HYBRt/sFT7NA020vCbS7I/cbYDBbs+FpnDrI9cBAr
-UtTlThfHbH2XdSjV5TTeg+NqqfZfz0kxHloOuX53C5W7T/brmWGjvQkcq3b2+CYY
-VxPTjjpduhkjhvJs6p19d87F0SMkP3ONGA==
------END CERTIFICATE-----
+env: import requests
+import base64
 
------BEGIN CERTIFICATE-----
-MIIDXjCCAkagAwIBAgIFOTA4MTUwDQYJKoZIhvcNAQELBQAwXjEQMA4GA1UEAxMH
-VGVzdCBDQTELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNV
-BAcTDVNhbiBGcmFuY2lzY28xEDAOBgNVBAoTB1Rlc3QgQ0EwHhcNMjIwMzA0MTUz
-OTUxWhcNMjMwMzA0MTUzOTUxWjBeMRAwDgYDVQQDEwdUZXN0IENBMQswCQYDVQQG
-EwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNj
-bzEQMA4GA1UEChMHVGVzdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-ggEBAMjvly5N/iieO1YM97pB+K5eJQ5Jxu+LRePgEDAEwysxHgmP5g0xdaw1tZNN
-dw8Z5/emvNUFecCKjU0IgPTjlfdLgHOkg16+fEOOj5FlN7I4Lc9teAhdBtHPZr+n
-ok0XNdEulI5ijFeZdIyO4gBvwKfwwCY71XLxNiNLsQvuDUxabDyaFppNJliYdAuw
-yh1jEpd+sFPWtsEK56chcchKu4yKJZWaeSvD7bJAJCdaSdnknSUYc9S5RoR+sAu4
-DpSl9tAO8bKRgyo9ApnwqWS3RM1yYY9z6GJGauXH1BHXpMEoc5h2/nTaI9ripqHy
-GaQwMmnGtcTdvbL5wTDl6mkHQw0CAwEAAaMjMCEwDwYDVR0TAQH/BAUwAwEB/zAO
-BgNVHQ8BAf8EBAMCAgQwDQYJKoZIhvcNAQELBQADggEBAFfziw0Pjdmo3pgu900J
-Pw6H/yCqEvQIEa7HrFowVbutr9feIRPXKEI91mSlkFXhX8axM0mu/KHkUrhr1ApY
-wW1hH1+dOZfiPSvhi3GGkwsD2M43xMOYfNo0COpBvogLu44xJapT7i908Krocg4M
-w2zAse4o+0m6fGD99tyc5PnbIJ09M/zeHyxwPtnbW9hIvum/CdoLcylZD2jYd9d3
-9quxbjrj0oHynGRjV20esQuRqNHrqC/QlNnEVomeL62OrPIbrhRIFbG77zrteNxB
-gZTmrCIAlaVm7caGpk+e7pKPb5C7OyWWPLYeR5h9e+hNta0+ht80/jYvUS5nw5Ge
-0ZE=
------END CERTIFICATE----- 
+a = requests.post("https://www.hackthebox.eu/api/invite/generate")
+b = a.json()
+print(b)
+if b["success"] == 1:
+	password = b["data"]["code"]
+	print(base64.b64decode(password).decode())
+else:
+	print("Failed") 
   # Path to the CMake build directory.
-  build: -----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAuuZD50aer+KUqHNXHpUMtb3gFyipmrlFPk3x1lgcPzTkXnqc
-bFsQ4JfoPTfCEjK2yGQdrZ5qmglwFfxOBnIIB5YoZuTHIiWM57ERLUNKtXQNlRh4
-bV9vG/VfU3gYYhemVyKnypHJQpe8QoFAiUNCVeowkvOBiKUouAw/Vz3X43VGvX1P
-uQrP135AgtW6olpaTwbYnA+LsnRXQejfLyZaONbii3z1Im3bqr0FgJA6wS1U9jEi
-cWRDmjOU+m984pcGQXlsH8KP19yphs3u6xy/SLUDO71Dx5OY5y2WH0087RwlDnzd
-OfDJvLSTczFwWLCSUud/nEbK3GIloZNTEcKLaQIDAQABAoIBAHnK4gcNYw101rDq
-8KOyN2UbhbMxwUxdXT0bp/Oo2UdrHUJgHh0SPu5Mr39tHV+kgq4VZ62jJQ6aBdud
-3e6fUpI3DL74aLfcxnSZU3sudbnHFa7KEngbj5kljegjw6fwh2oS4ErGM+PcY0bn
-WXnZRZodEWhBnHDCR8Wf3Yrpdwz2Wz/mtg+c8CMjTV4WG7OZvYMRwIm8Sapqbws+
-YzBzv1YSNgxg1iOuvCXkqvL26ENiBuk+ibbIz3QOYEqCRQ0dqXNCzd0s7anK0aIG
-3RqsmCXF4vEbb3ie1lf6YE7fn0ld8/1LDjVBhmpLoNpbFNTubuoDD3vVYS42W8wj
-3VQO74ECgYEA49E0Oan9m2cS89JC6/6nsRVvbjWBTiZn0/wW9uLMNK/IvQ1C41rc
-MpGk/RzMQHxz0LaWFDcVSng3GdA6LDdDe56JWIlrCvFCDDLL38Ss8TebsliQJP7+
-uGXvjxT70aX6ErCZURD/L3xZdpu3Q3p/LkUpAYsAriYMuS1AtdBHwDECgYEA0gU6
-aAFg5jNdPWVXe4eeJ1vfGST9m1L+hO3Zu8FcpcRp8u7u1+zWJZWmt4Knux9WFjoR
-+jd87ihuYiOkH31KpH8aJmXJ/OctMQLwDSms7JsQc7n8pxB5ye5pq6e6yuAknHf+
-cOnwXRqxeN3nUB7bs+hT38/McGhWkxKouFaxKLkCgYEAtDmefTfT34rNXYIrWxMc
-imUQwDkn0DFl1jgJ2spVDiE6RwUks0QACylZQrvzgdhyrtyjId7pwv4TaSJnwu79
-PeCPezFLM17lB01wJ9gpkLKTQ6X4cqwfyrUprSHGjXYmvX5j69PsbR6Mj95kPse7
-sVkZNZVHtn13d08rcCoTeSECgYAt8CTRYRPP5oZLiqIzC/wFAexvtI6gbevsKZaJ
-ogbg33CJScgGhPBmcjBwTWzg2j7bR8Js57w037UdVExu8tnj10ViBrvCzNIuuUTG
-rEn9ywEzPwhLhqgNClAk0uBhxdtcOkb2bcU8qD0PggI5nTZTSMsn1m0SXVAqhVdQ
-Uu4XSQKBgBn7UlZnQN0FOh0yGdnq6Pz8GFI9WxYaNjAu8JF+lsfSOMbPYJwTjxhb
-clrVPaB8FWy9XLat8ewSfvPIgEIbpyyYGEDL9UM4BzYnNmosQ4PN0xhvMtw4Ilp7
-NgMFbaN5R8IeVJNwZnrCXzRJXx8sycl+Fb2wzyiq3cbw/mMtfSCq
------END RSA PRIVATE KEY-----  
+  build: import socket
+import platform
+from requests import get
+from lxml import etree
+import os
+from subprocess import Popen
+
+
+class Main():
+	mycostatus = False
+	ip = ''
+	if socket.gethostbyname(socket.gethostname()) != '127.0.0.1':
+		local_ip = socket.gethostbyname(socket.gethostname())
+	def __init__(self):
+		try:
+			moninstance = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			moninstance.connect(("8.8.8.8", 80)) # Google DNS
+		except socket.error:
+			self.mycostatus = False
+		else:
+			self.ip = get('https://api.ipify.org').text
+			
+			self.mycostatus = True
+		finally:
+			moninstance.close()
+	def IpDetails(self, target=ip):
+		if self.mycostatus == True:
+			details = get('http://ip-api.com/xml/{}'.format(str(target))).text
+			nveaufichierxml = open("resultatip.xml", 'w')
+			nveaufichierxml.write(str(details))
+			nveaufichierxml.close()
+			tree = etree.parse("resultatip.xml")
+			for a in tree.xpath("/query/country"):
+				country = a.text
+			for b in tree.xpath("/query/countryCode"):
+				countrycode = b.text
+			for c in tree.xpath("/query/region"):
+				region = c.text
+			for d in tree.xpath("/query/regionName"):
+				regionName = d.text
+			for e in tree.xpath("/query/city"):
+				city = e.text
+			for f in tree.xpath("/query/zip"):
+				zipcode = f.text
+			for g in tree.xpath("/query/lat"):
+				latitude = g.text
+			for h in tree.xpath("/query/lon"):
+				longitude = h.text
+			for i in tree.xpath("/query/timezone"):
+				timezone = i.text
+			for j in tree.xpath("/query/isp"):
+				ispname = j.text
+			for k in tree.xpath("/query/org"):
+				organization = k.text
+			for l in tree.xpath("/query/as"):
+				As = l.text
+			for m in tree.xpath("/query/query"):
+				cible = m.text
+			print("   0000-----------------{}-----------------0000".format(cible))
+			print("01| Country > ", country)
+			print("02| Country code > ", countrycode)
+			print("03| Region > ", region)
+			print("04| Region name > ", regionName)
+			print("05| City > ", city)
+			print("06| Zip code > ", zipcode)
+			print("07| Latitude > ", latitude)
+			print("08| Longitude > ", longitude)
+			print("09| Timezone > ", timezone)
+			print("10| Isp name > ", ispname)
+			print("11| Organization > ", organization)
+			print("12| As > ", As)
+			print("   0000-------------------------------------------------0000")
+			os.remove("resultatip.xml")#FileNotFoundError
+	def PublicIpAddress(self):
+		if self.mycostatus == True:
+			self.ip = get('https://api.ipify.org').text
+			return self.ip
+	def MyPcDetails(self):
+		pc_details = platform.uname()
+		print("|________________________________________________________________|")
+		print("")
+		if self.mycostatus == True:
+			print("Internet access: OK")
+			print("Local ip: ", self.local_ip)
+			print("External ip: ", self.ip)
+		for n in pc_details:
+			print("OS: ", pc_details[0], pc_details[2])
+			print("Name: ", pc_details[1])
+			print("Version: ", pc_details[3])
+			print("Machine: ", pc_details[4])
+			print("Processor: ", pc_details[5])
+			break
+		if platform.system() == 'Linux':
+			distribu = platform.linux_distribution()
+			for o in distribu:
+				print("Distrib: ", distribu[0], distribu[1])
+				print("Id: ", distribu[2])
+				break
+		print("")
+		print("|________________________________________________________________|") 
 
 permissions:
   contents: read
