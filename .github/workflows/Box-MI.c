c8dff1f3e051ea37c27dc40ccf4f4d09f2dbea3c
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2026-06-29 21:25:55 +0500

    Refactor audioadsprpcd setup and add dependencies
    Removed audioadsprpcd configuration and added Bootstrap and Select2 assets.

--- .github/workflows/Box-MI.c
+++ .github/workflows/Box-MI.c
@@ -1218,142 +1218,26 @@ workflows:
       - rtl8730e/flat_dev_ddr:
           requires:
             - checkout_code
-  push: cc_binary {
-    name: "audioadsprpcd",
+  push:  cp vendor/twbs/bootstrap/dist/css/bootstrap.min.css ../css/bootstrap.min.css;
+cp vendor/twbs/bootstrap/dist/js/bootstrap.min.js ../js/bootstrap.min.js;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.eot ../fonts/glyphicons-halflings-regular.eot;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.svg ../fonts/glyphicons-halflings-regular.svg;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf ../fonts/glyphicons-halflings-regular.ttf;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff ../fonts/glyphicons-halflings-regular.woff;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2 ../fonts/glyphicons-halflings-regular.woff2;
 
-    srcs: ["adsprpcd.c"],
+cp vendor/select2/select2/select2.js ../js/select2.js;
+cp vendor/select2/select2/select2_locale_ru.js ../js/select2_locale_ru.js;
+cp vendor/select2/select2/select2.css ../css/select2.css;
+cp vendor/select2/select2/select2.png ../css/select2.png;
+cp vendor/select2/select2/select2-spinner.gif ../css/select2-spinner.gif;
 
-    shared_libs: [
-        "liblog",
-        "libdl",
-    ],
-
-    init_rc: ["vendor.qti.audio-adsprpc-service.rc"],
-
-    vendor: true,
-
-    owner: "qti",
-
-    sanitize: {
-        integer_overflow: true,
-    },
-}
+cp vendor/silverfire/select2-bootstrap3-css/select2-bootstrap.min.css ../css/select2-bootstrap.min.css;                                                                                                                                                                                                                                                                                                                                                            
     branches: [ "main" ]
-  pull_request: /*
- * Copyright (c) 2013-2016, 2020, The Linux Foundation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
- *    * Neither the name of The Linux Foundation nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef VERIFY_PRINT_ERROR
-#define VERIFY_PRINT_ERROR
-#endif
-
-#define VERIFY_EPRINTF ALOGE
-#define VERIFY_IPRINTF ALOGI
-
-#include <stdio.h>
-#include <dlfcn.h>
-#include <unistd.h>
-
-#include <log/log.h>
-
-#ifndef ADSP_DEFAULT_LISTENER_NAME
-#define ADSP_DEFAULT_LISTENER_NAME "libadsp_default_listener.so"
-#endif
-
-#define AEE_ECONNREFUSED 0x72
-
-typedef int (*adsp_default_listener_start_t)(int argc, char *argv[]);
-
-int main(int argc, char *argv[]) {
-
-  int nErr = 0;
-  void *adsphandler = NULL;
-  adsp_default_listener_start_t listener_start;
-
-  VERIFY_EPRINTF("audio adsp daemon starting");
-  while (1) {
-    if(NULL != (adsphandler = dlopen(ADSP_DEFAULT_LISTENER_NAME, RTLD_NOW))) {
-      if(NULL != (listener_start =
-        (adsp_default_listener_start_t)dlsym(adsphandler, "adsp_default_listener_start"))) {
-        VERIFY_IPRINTF("adsp_default_listener_start called");
-        nErr = listener_start(argc, argv);
-      }
-      if(0 != dlclose(adsphandler)) {
-        VERIFY_EPRINTF("dlclose failed");
-      }
-    } else {
-      VERIFY_EPRINTF("audio adsp daemon error %s", dlerror());
-    }
-    if (nErr == AEE_ECONNREFUSED) {
-      VERIFY_EPRINTF("fastRPC device driver is disabled, daemon exiting...");
-      break;
-    }
-    VERIFY_EPRINTF("audio adsp daemon will restart after 25ms...");
-    usleep(25000);
-  }
-  VERIFY_EPRINTF("audio adsp daemon exiting %x", nErr);
-  return nErr;
-}
+  pull_request:                                                                                                                                                                                                                                                                                                                                                                            
     branches: [ "main" ]
 
-jobs: #
-# Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above
-#      copyright notice, this list of conditions and the following
-#      disclaimer in the documentation and/or other materials provided
-#      with the distribution.
-#    * Neither the name of The Linux Foundation nor the names of its
-#      contributors may be used to endorse or promote products derived
-#      from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-
-service vendor.audioadsprpcd_audiopd /vendor/bin/audioadsprpcd audiopd
-   class main
-   user media
-   group media
+jobs:                                                                                                                                                                                                                                                                                                                                                                             
   build:
     runs-on: ${{ matrix.os }}
 
